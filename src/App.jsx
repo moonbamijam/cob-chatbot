16729e739d94fa3a32008ea5fdc55b914625bb5f
MoonbamiOfficial <141120384+MoonbamiOfficial@users.noreply.github.com>
2024-03-15 16:46:54 +0800

    refactor: create dummy page component

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useRef, useEffect } from "react";
-import Section from "./components/common/Section";
 import ChatHead from "./components/bot/ChatHead";
 import MessageBox from "./components/bot/MessageBox";
+import DummyPage from "./components/DummyPage";
 
 const App = () => {
   const [isChatActive, setIsChatActive] = useState(false);
@@ -22,46 +22,7 @@ const App = () => {
           <ChatHead state={isChatActive} setState={setIsChatActive} />
           {isChatActive && <MessageBox />}
         </div>
-        <div
-          id="main-logo"
-          className="my-4 rounded-full w-[100px] h-[100px] bg-dummyhighlight"
-        ></div>
-        <Section>
-          <div
-            id="header-wrapper"
-            className="flex justify-between items-center px-8 lg:px-14 pb-8"
-          >
-            <div
-              id="sub-logo"
-              className="rounded-md w-16 h-12 bg-dummyhighlight"
-            ></div>
-            <div
-              id="menu"
-              className="w-[350px] h-[40px] bg-dummysecondary"
-            ></div>
-            <div
-              id="search"
-              className="rounded-xl w-[120px] h-[20px] bg-dummyextra"
-            ></div>
-          </div>
-          <div
-            id="content-panel-main"
-            className="w-full h-[450px] bg-dummyprimary"
-          ></div>
-          <div
-            id="sub-content-panel-wrapper"
-            className="flex justify-evenly px-8 lg:px-14 mt-12"
-          >
-            <div
-              id="content-panel-2"
-              className="w-[450px] h-[250px] bg-dummysecondary"
-            ></div>
-            <div
-              id="content-panel-2"
-              className="w-[220px] h-[250px] bg-dummyprimary"
-            ></div>
-          </div>
-        </Section>
+        <DummyPage />
         {isChatActive && (
           <div
             id="screen-dimmer"
