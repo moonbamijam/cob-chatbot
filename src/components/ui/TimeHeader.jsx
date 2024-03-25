38b5ba583f955542743201b76409aa8fbec262b1
MoonbamiOfficial <141120384+MoonbamiOfficial@users.noreply.github.com>
2024-03-25 18:51:39 +0800

    feat: add time header

---
+++ src/components/ui/TimeHeader.jsx
@@ -0,0 +1,19 @@
+import { formatedDate } from "../../utils/date";
+
+const TimeHeader = () => {
+  return (
+    <>
+      <div
+        id="date-header"
+        className="w-full relative flex gap-10 text-xs py-1 text-white font-bold uppercase"
+      >
+        <div id="city" className="">
+          <p>bacoor</p>
+        </div>
+        <p id="date">{formatedDate}</p>
+      </div>
+    </>
+  );
+};
+
+export default TimeHeader;
