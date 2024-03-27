2ee4b0a8f4635ba8bb359044293fc95d1a8c9b3d
MoonbamiOfficial <141120384+MoonbamiOfficial@users.noreply.github.com>
2024-03-27 18:03:26 +0800

    feat: add highlight title ui component

---
+++ src/components/ui/HighlightTitle.jsx
@@ -0,0 +1,9 @@
+const HighlightTitle = ({ text }) => {
+  return (
+    <div className="bg-[#ff0202] px-3 capitalize text-white font-semibold text-sm rounded mr-auto inline">
+      {text}
+    </div>
+  );
+};
+
+export default HighlightTitle;
