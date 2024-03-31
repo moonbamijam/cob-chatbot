8df79b3b22029ffba87b54f4e5dcc788d22f2a9d
MoonbamiOfficial <141120384+MoonbamiOfficial@users.noreply.github.com>
2024-03-31 23:24:13 +0800

    feat: new ui for chat component

--- src/components/common/Chat.jsx
+++ src/components/common/Chat.jsx
@@ -1,28 +1,43 @@
-import { RiRobot2Fill } from "react-icons/ri";
+import { chatbot } from "../../libs/bot-details";
 
-const Chat = ({ id, isBot, message }) => {
+const Chat = ({ role, message, timeSent }) => {
   return (
-    <>
-      {isBot ? (
-        <div className="w-full relative flex items-center gap-2 mr-auto">
-          <span id="chatbot-icon" className="text-2xl">
-            <RiRobot2Fill />
-          </span>
-          <p className="max-w-[80%] rounded-3xl bg-gray-500 text-white px-4 py-3 my-2 break-words">
-            {message}
-          </p>
-        </div>
-      ) : (
-        <div className="w-full flex justify-end">
-          <p
-            className="max-w-[80%] rounded-3xl bg-dummyhighlight
-            text-white px-4 py-3 my-2 break-words"
+    <div className="">
+      <div
+        className={`${
+          role == "bot"
+            ? "w-full relative flex items-center gap-2"
+            : "w-full flex justify-end"
+        }  mt-4 mb-2`}
+      >
+        {role == "bot" ? (
+          <img
+            src={chatbot.logo}
+            alt="Chatbot Icon"
+            width={35}
+            height={35}
+            className="rounded-full"
+          />
+        ) : (
+          ""
+        )}
+        <div
+          className={`max-w-[80%] rounded-3xl ${
+            role == "bot" ? "bg-gray-500" : "bg-highlight"
+          } text-white px-4 py-3 break-words`}
+        >
+          <p id="message">{message}</p>
+          <div
+            id="timeSent"
+            className={`w-full ${
+              role == "bot" ? "text-right" : "text-left"
+            } text-xs text-gray-300`}
           >
-            {message}
-          </p>
+            {timeSent}
+          </div>
         </div>
-      )}
-    </>
+      </div>
+    </div>
   );
 };
 
