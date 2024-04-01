12deb90baeaa2142af411f2ec4e3b7a830f20872
MoonbamiOfficial <141120384+MoonbamiOfficial@users.noreply.github.com>
2024-04-01 17:54:05 +0800

    feat: add loading ui component

---
+++ src/components/ui/Loading.jsx
@@ -0,0 +1,11 @@
+import { AiOutlineLoading3Quarters } from "react-icons/ai";
+
+const Loading = () => {
+  return (
+    <div className="flex justify-center ">
+      <AiOutlineLoading3Quarters className="text-4xl animate-spin" />
+    </div>
+  );
+};
+
+export default Loading;
