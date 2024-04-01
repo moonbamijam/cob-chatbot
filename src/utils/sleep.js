005fc062f5fa65f3ed8ab08bfebe4330fff5c4e5
MoonbamiOfficial <141120384+MoonbamiOfficial@users.noreply.github.com>
2024-04-01 17:54:58 +0800

    chore: add guide comment to sleep.js

--- src/utils/sleep.js
+++ src/utils/sleep.js
@@ -1,3 +1,4 @@
 export const sleep = async (seconds) => {
   return new Promise((resolve) => setTimeout(resolve, seconds * 1000));
 };
+// Function that will create a delay based on the number of the given param
