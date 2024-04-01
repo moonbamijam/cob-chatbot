6925a219bfecfa917c9ac8ba33a782a3036a6d5c
MoonbamiOfficial <141120384+MoonbamiOfficial@users.noreply.github.com>
2024-04-01 17:55:29 +0800

    feat: add chatbot details object to use around

---
+++ src/libs/bot-details.js
@@ -0,0 +1,7 @@
+import chatbotLogo from "../assets/bacoor-mayor-vmayor.jpg";
+
+export const chatbot = {
+  logo: chatbotLogo,
+  name: "Bacooreño Bot",
+  slogan: "Tanong mo, sagot ko, dahil at home ka dito!",
+};
