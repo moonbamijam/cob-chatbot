c83c9b25449407d34e49da4f331514ac955b488f
MoonbamiOfficial <141120384+MoonbamiOfficial@users.noreply.github.com>
2024-04-01 17:56:47 +0800

    feat: add new functionality to chat in message box
    Closes #17

--- src/components/bot/MessageBox.jsx
+++ src/components/bot/MessageBox.jsx
@@ -1,53 +1,151 @@
-import { RiRobot2Fill } from "react-icons/ri";
-import { IoSend } from "react-icons/io5";
-import Chat from "../common/Chat";
+import { useEffect, useRef, useState } from "react";
+import { db } from "../../utils/firebase-config";
+import {
+  collection,
+  getDocs,
+  addDoc,
+  Timestamp,
+  query,
+  orderBy,
+} from "firebase/firestore";
 import TextareaAutosize from "react-textarea-autosize";
-import { useState } from "react";
+
+// Libraries
+import { chatbot } from "../../libs/bot-details";
+import { timeDisplay } from "../../utils/time-display";
+
+// Utilities
+import { sleep } from "../../utils/sleep";
+
+// Components
+import Chat from "../common/Chat";
+import Typing from "../ui/Typing";
+
+// Icons
+import { IoSend } from "react-icons/io5";
+import Loading from "../ui/Loading";
 
 const MessageBox = () => {
-  const [botMessage, setBotMessage] = useState("");
-  const [userMessage, setUserMessage] = useState("");
+  const scrollIntoNewChat = useRef();
 
-  const sendMessage = async (event, message) => {
-    try {
-      event.preventDefault();
-      const response = await fetch(
-        `http://localhost:3001/bot?message=${encodeURIComponent(message)}`
-      );
-      const data = await response.text();
-      console.log("data: ", data);
-      setBotMessage(data.toString());
-      setUserMessage("");
-    } catch (error) {
-      console.log("failed: ", error);
-    }
+  const [loading, setLoading] = useState(true);
+  const [userMessage, setUserMessage] = useState("");
+  const [botMessage, setBotMessage] = useState("initial text");
+  const [botIsTyping, setBotIsTyping] = useState(false);
+  const [messages, setMessages] = useState([]);
+
+  const messagesCollectionRef = collection(db, "messages");
+  const q = query(messagesCollectionRef, orderBy("timeSent", "asc"));
+
+  const getReplyFromBot = async (message) => {
+    setBotIsTyping(true);
+    await sleep(3);
+    const response = await fetch(
+      `http://localhost:3001/bot?message=${encodeURIComponent(message)}`
+    );
+    const data = await response.json();
+    setBotIsTyping(false);
+    await addDoc(messagesCollectionRef, {
+      text: data,
+      role: "bot",
+      timeSent: Timestamp.now(),
+    });
+    setBotMessage(data)
   };
+
+  const sendMessageToBot = async (event, message) => {
+    event.preventDefault();
+    setUserMessage("");
+    await addDoc(messagesCollectionRef, {
+      text: userMessage,
+      role: "user",
+      timeSent: Timestamp.now(),
+    });
+    await sleep(1.5);
+    await getReplyFromBot(message);
+  };
+
+  const getChatHistory = async () => {
+    const data = await getDocs(q);
+    setMessages(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
+    setLoading(false);
+  };
+
+  useEffect(() => {
+    scrollIntoNewChat.current?.scrollIntoView({ behavior: "smooth" });
+  }, [messages, botIsTyping]);
+
+  useEffect(() => {
+    getChatHistory();
+  }, [userMessage, botMessage]);
 
   return (
     <div
       id="message-box"
-      className="fixed right-10 bottom-24 sm:right-20 lg:right-28 lg:bottom-28 w-[400px] sm:w-[500px] h-[600px] sm:h-[700px] bg-white rounded-lg z-[100]"
+      className="fixed flex flex-col right-10 bottom-24 sm:right-20 lg:right-28 lg:bottom-32 w-[400px] sm:w-[500px] h-[600px] sm:h-[700px] bg-white rounded-lg overflow-hidden z-[100]"
     >
       <header
         id="chat-ui-header"
-        className="flex items-center gap-4 px-8 py-4 mr-auto shadow-md"
+        className="w-full flex items-center justify-between px-8 py-4 mr-auto shadow-md"
       >
-        <span className="text-3xl">
-          <RiRobot2Fill />
-        </span>
-        <h3 className="text-2xl capitalize font-semibold">
-          city of bacoor - chatbot
-        </h3>
+        <div className="flex items-center gap-4">
+          <img
+            src={chatbot.logo}
+            alt=""
+            width={45}
+            height={45}
+            className="rounded-full border border-black"
+          />
+          <h3 className="text-2xl capitalize font-semibold">{chatbot.name}</h3>
+        </div>
+        <p id="timenow" className="text-lg">
+          {timeDisplay()}
+        </p>
       </header>
-      <section id="messages" className="px-4 w-full mt-[100px]">
-        {userMessage && <Chat message={userMessage} />}
-        {botMessage && <Chat isBot={true} message={botMessage} />}
+      <section
+        id="messages"
+        className="px-4 py-6 w-full h-full overflow-y-scroll scroll-ml-6 no-scrollbar"
+      >
+        <div id="bot-profile" className="flex flex-col items-center mb-8">
+          <img
+            src={chatbot.logo}
+            alt=""
+            width={100}
+            height={100}
+            className="rounded-full border border-black mb-2"
+          />
+          <div id="details" className="text-center mb-4">
+            <h1 className="font-bold text-4xl mb-1">{chatbot.name}</h1>
+            <p className="">{chatbot.slogan}</p>
+          </div>
+          <hr className="w-full" />
+        </div>
+        {loading ? (
+          <Loading />
+        ) : (
+          <div>
+            {messages.map((message, id) => (
+              <Chat
+                key={id}
+                role={message.role}
+                message={message.text}
+                timeSent={new Date(message.timeSent.seconds * 1000)
+                  .toLocaleTimeString()
+                  .replace(/(.*)\D\d+/, "$1")}
+              />
+            ))}
+            {botIsTyping && <Typing />}
+          </div>
+        )}
+        <div ref={scrollIntoNewChat}></div>
       </section>
       <form
         action=""
         method=""
-        onSubmit={(e) => sendMessage(e, userMessage)}
-        className="w-full absolute bottom-0 flex justify-between items-center gap-2 px-4 py-4"
+        onSubmit={(e) => {
+          sendMessageToBot(e, userMessage);
+        }}
+        className="w-full flex justify-between items-center gap-2 px-4 py-2"
       >
         <TextareaAutosize
           name="chat"
