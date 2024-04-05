8fa12866ef25c9360d3b0ef882e8b56717afa4f8
MoonbamiOfficial <141120384+MoonbamiOfficial@users.noreply.github.com>
2024-04-05 19:31:30 +0800

    chore: use production firebase config

--- src/utils/firebase-config.js
+++ src/utils/firebase-config.js
@@ -6,25 +6,25 @@ import { getFirestore } from "firebase/firestore";
 // REMINDER!: please set up the .env variables before running in dev
 
 // PRODUCTION
-// const firebaseConfig = {
-//   apiKey: `${import.meta.env.APIKEY}`,
-//   authDomain: `${import.meta.env.AUTHDOMAIN}`,
-//   projectId: "cob-chatbot",
-//   storageBucket: `${import.meta.env.STORAGEBUCKET}`,
-//   messagingSenderId: `${import.meta.env.MESSAGINGSENDERID}`,
-//   appId: `${import.meta.env.APPID}`,
-// };
+const firebaseConfig = {
+  apiKey: `${import.meta.env.APIKEY}`,
+  authDomain: `${import.meta.env.AUTHDOMAIN}`,
+  projectId: "cob-chatbot",
+  storageBucket: `${import.meta.env.STORAGEBUCKET}`,
+  messagingSenderId: `${import.meta.env.MESSAGINGSENDERID}`,
+  appId: `${import.meta.env.APPID}`,
+};
 
 // DEVELOPMENT
-const firebaseConfig = {
-  apiKey: `${import.meta.env.APIKEYDEV}`,
-  authDomain: `${import.meta.env.AUTHDOMAINDEV}`,
-  projectId: "chatbot-test-49529",
-  storageBucket: `${import.meta.env.STORAGEBUCKETDEV}`,
-  messagingSenderId: `${import.meta.env.MESSAGINGSENDERIDDEV}`,
-  appId: `${import.meta.env.APPIDDEV}`,
-  measurementId: `${import.meta.env.MEASUREMENTIDDEV}`,
-};
+// const firebaseConfig = {
+//   apiKey: `${import.meta.env.APIKEYDEV}`,
+//   authDomain: `${import.meta.env.AUTHDOMAINDEV}`,
+//   projectId: "chatbot-test-49529",
+//   storageBucket: `${import.meta.env.STORAGEBUCKETDEV}`,
+//   messagingSenderId: `${import.meta.env.MESSAGINGSENDERIDDEV}`,
+//   appId: `${import.meta.env.APPIDDEV}`,
+//   measurementId: `${import.meta.env.MEASUREMENTIDDEV}`,
+// };
 
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
