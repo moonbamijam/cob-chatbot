cb955e735ad3b8c1d764625a35ec8a394c96f9f1
MoonbamiOfficial <141120384+MoonbamiOfficial@users.noreply.github.com>
2024-04-08 15:23:42 +0800

    feat: add error handling ui component

---
+++ src/components/ui/Error.jsx
@@ -0,0 +1,11 @@
+const Error = () => {
+  return (
+    <div className="w-full flex justify-center">
+      <div className="min-w-[50%] px-4 py-3 mt-3 mb-2 text-center text-white rounded-3xl bg-red-500">
+        Something went wrong!
+      </div>
+    </div>
+  );
+}
+
+export default Error
