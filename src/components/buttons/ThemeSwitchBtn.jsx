545223dfe301e4c1d0df6bce9d50303ebde279c1
MoonbamiOfficial <141120384+MoonbamiOfficial@users.noreply.github.com>
2024-04-09 18:01:20 +0800

    perf: move toggleTheme fn and add themes context

--- src/components/buttons/ThemeSwitchBtn.jsx
+++ src/components/buttons/ThemeSwitchBtn.jsx
@@ -1,55 +1,60 @@
+import { useContext } from "react";
 import { LuSun } from "react-icons/lu";
 import { FaMoon } from "react-icons/fa6";
 
-const ThemeSwitchBtn = ({ state, onClick }) => {
+import { ThemesContext } from "../../providers/ThemesProvider";
+
+const ThemeSwitchBtn = () => {
+  const [theme, setTheme] = useContext(ThemesContext);
+
+  const toggleTheme = () => {
+    setTheme(theme === "dark" ? "light" : "dark");
+  };
+
   return (
-    <>
+    <div className="flex items-center justify-center my-2 overflow-hidden">
       <input type="checkbox" name="toggle" id="toggle" className="hidden" />
       <button
-        onClick={() => onClick()}
-        className="display toggle-wrapper flex items-center justify-center gap-1 my-2 overflow-hidden"
+        onClick={() => toggleTheme()}
+        className="display toggle-wrapper flex items-center justify-center gap-1"
       >
         <ThemeName text={"light"} />
         <label
           htmlFor="toggle"
           className={`relative border rounded-full w-[130px] h-[60px] shadow-inner cursor-pointer [&>div]:active:w-[55px] ${
-            state === "dark"
+            theme === "dark"
               ? "bg-gray-500 shadow-gray-800 border-transparent"
-              : "bg-gray-200 shadow-gray-300"
+              : "bg-gray-300 shadow-gray-400"
           }`}
         >
           <div
-            className={` w-[50px] h-[50px] absolute top-[50%] left-0 translate-y-[-50%] flex justify-center items-center shadow-inner bg-white rounded-full ${
-              state === "dark"
+            className={` w-[50px] h-[50px] absolute top-[50%] left-0 translate-y-[-50%] flex justify-center items-center shadow-inner rounded-full ${
+              theme === "dark"
                 ? "left-full translate-x-[-110%] bg-gray-700 shadow-gray-800"
-                : "translate-x-[10%] "
+                : "translate-x-[10%] bg-white shadow-gray-400"
             }`}
           >
             <LuSun
               className={`absolute w-[30px] h-[30px]  text-yellow-500 ${
-                state === "dark" ? "mt-[150%] opacity-0" : "mt-0 opacity-100"
+                theme === "dark" ? "mt-[150%] opacity-0" : "mt-0 opacity-100"
               } `}
             />
             <FaMoon
-              className={`absolute w-[30px] h-[30px]  text-white ${
-                state === "dark" ? "mt-0 opacity-100" : "mt-[-150%] opacity-0"
+              className={`absolute w-[30px] h-[30px] text-white ${
+                theme === "dark" ? "mt-0 opacity-100" : "mt-[-150%] opacity-0"
               } `}
             />
           </div>
         </label>
         <ThemeName text={"dark"} />
       </button>
-    </>
+    </div>
   );
 };
 
 const ThemeName = ({ text }) => {
   return (
-    <p
-      className={`capitalize dark:text-white rounded-3xl  px-4 py-3 `}
-    >
-      {text}
-    </p>
+    <p className={`capitalize dark:text-white rounded-3xl px-4 py-3`}>{text}</p>
   );
 };
 export default ThemeSwitchBtn;
--- src/utils/toggle-theme.js
+++
@@ -1,3 +0,0 @@
-export const toggleTheme = (state, setState) => {
-  setState(state === "dark" ? "light" : "dark");
-};
