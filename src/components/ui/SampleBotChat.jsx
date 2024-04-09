3973a94ef3ac47fff536347dcf1ff5520ea97ace
MoonbamiOfficial <141120384+MoonbamiOfficial@users.noreply.github.com>
2024-04-09 18:00:12 +0800

    feat: add sample user and bot chat ui component

---
+++ src/components/ui/SampleBotChat.jsx
@@ -0,0 +1,30 @@
+import { chatbot } from "../../libs/bot-details";
+
+const SampleBotChat = ({ style }) => {
+  return (
+    <div style={style} className="bot message">
+      <div className="w-full relative flex items-center gap-2 mt-3 mb-2">
+        <img
+          src={chatbot.logo}
+          alt="Chatbot Icon"
+          width={35}
+          height={35}
+          className="rounded-full"
+        />
+        <div className="max-w-[80%] rounded-3xl bg-bot dark:bg-gray-600  px-4 py-3 break-words">
+          <p id="message" className="dark:text-white">
+            Lorem ipsum dolor sit.
+          </p>
+          <div
+            id="timeSent"
+            className="w-full text-gray-500 dark:text-gray-300 text-xs text-right"
+          >
+            8:15 PM
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default SampleBotChat;
