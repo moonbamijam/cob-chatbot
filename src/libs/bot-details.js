90586b36cea9ea2b0d4db5b3e4cc2a83dfe83fb8
MoonbamiOfficial <141120384+MoonbamiOfficial@users.noreply.github.com>
2024-04-11 00:07:26 +0800

    config: remove extra letter in bot name

--- src/libs/bot-details.js
+++ src/libs/bot-details.js
@@ -2,8 +2,8 @@ import chatbotLogo from "../assets/bacoor-mayor-vmayor.jpg";
 
 export const chatbot = {
   logo: chatbotLogo,
-  name: "Vivianne Chantelle",
-  nickName: "Vivianne",
+  name: "Viviane Chantelle",
+  nickName: "Viviane",
   slogan: "Tanong mo, sagot ko. Dahil at home ka dito!",
   desc: "The Chatbot for the City of Bacoor, Cavite",
 };
