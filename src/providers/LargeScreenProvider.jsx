2822be27237e6f9502ed685afa9a0bb687de37b5
MoonbamiOfficial <141120384+MoonbamiOfficial@users.noreply.github.com>
2024-04-11 00:08:29 +0800

    feat: add persistence for the size of chat box

---
+++ src/providers/LargeScreenProvider.jsx
@@ -0,0 +1,31 @@
+import React, { useEffect, useState } from "react";
+
+export const LargeScreenContext = React.createContext();
+
+const parsedIsLargeScreen = localStorage.getItem("isLargeScreen");
+
+const LargeScreenProvider = ({ children }) => {
+  const [isLargeScreen, setIsLargeScreen] = useState(
+    parsedIsLargeScreen === "true"
+  );
+
+  useEffect(() => {
+    if (localStorage.getItem("isLargeScreen") == null) {
+      const initialScreenSize = false;
+      setIsLargeScreen(initialScreenSize);
+      localStorage.setItem("isLargeScreen", initialScreenSize);
+    }
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem("isLargeScreen", isLargeScreen);
+  }, [isLargeScreen]);
+
+  return (
+    <LargeScreenContext.Provider value={[isLargeScreen, setIsLargeScreen]}>
+      {children}
+    </LargeScreenContext.Provider>
+  );
+};
+
+export default LargeScreenProvider;
