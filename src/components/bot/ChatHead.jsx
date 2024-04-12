c58a6098a9acbdc05a46451851a15ab46f5bf4c0
MoonbamiOfficial <141120384+MoonbamiOfficial@users.noreply.github.com>
2024-04-12 16:55:00 +0800

    feat: add chat head and chat box animation
    Closes #11

--- src/components/bot/ChatHead.jsx
+++ src/components/bot/ChatHead.jsx
@@ -7,17 +7,11 @@ const ChatHead = ({ state, onClick }) => {
       <button
         onClick={() => onClick()}
         id=""
-        className="fixed right-8 bottom-8 lg:right-12 lg:bottom-12 rounded-full p-3 text-3xl lg:text-4xl text-white z-[100] "
+        className={`fixed right-12 bottom-12 rounded-full p-3 text-3xl lg:text-4xl text-white z-[100] ${
+          state ? "opacity-0 translate-y-[100%] invisible" : ""
+        }`}
       >
-        {state ? (
-          <div className="flex justify-evenly bg-highlight rounded-full p-3">
-            <IoClose />
-          </div>
-        ) : (
-          <>
-            <img src={Mascot} alt="" className="w-[150px] " />
-          </>
-        )}
+        <img src={Mascot} alt="" className="w-[250px]" />
       </button>
     </>
   );
