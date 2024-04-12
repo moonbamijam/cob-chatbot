692790f18e8314d3d607fd4651465878b53f9a5a
MoonbamiOfficial <141120384+MoonbamiOfficial@users.noreply.github.com>
2024-04-12 17:12:51 +0800

    perf: remove unused styling to chat head

--- src/components/bot/ChatHead.jsx
+++ src/components/bot/ChatHead.jsx
@@ -7,7 +7,7 @@ const ChatHead = ({ state, onClick }) => {
       <button
         onClick={() => onClick()}
         id=""
-        className={`fixed right-12 bottom-12 rounded-full p-3 text-3xl lg:text-4xl text-white z-[100] ${
+        className={`fixed right-12 bottom-12 rounded-full z-[100] ${
           state ? "opacity-0 translate-y-[100%] invisible" : ""
         }`}
       >
