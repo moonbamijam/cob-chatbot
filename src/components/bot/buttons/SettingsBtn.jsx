146673883e3c95fccf9924be8046d11db709d98f
MoonbamiOfficial <141120384+MoonbamiOfficial@users.noreply.github.com>
2024-04-12 17:00:54 +0800

    refactor: move dark styling directly to react icon

--- src/components/bot/buttons/SettingsBtn.jsx
+++ src/components/bot/buttons/SettingsBtn.jsx
@@ -4,11 +4,13 @@ const SettingsBtn = ({ state, onClick }) => {
   return (
     <button
       onClick={() => onClick()}
-      className={`rounded-full p-4 hover:bg-highlight [&>svg>path]:hover:text-white [&>svg>path]:dark:text-white ${
+      className={`rounded-full p-4 hover:bg-highlight [&>svg>path]:hover:text-white  ${
         state && "bg-highlight [&>svg>path]:text-white"
       }`}
     >
-      <BsThreeDotsVertical className={`${state && "rotate-90"}`} />
+      <BsThreeDotsVertical
+        className={`${state && "rotate-90"} dark:text-white`}
+      />
     </button>
   );
 };
