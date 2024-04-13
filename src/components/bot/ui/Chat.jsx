0a0ed847ee5327c43299455382be128e1f944a13
MoonbamiOfficial <141120384+MoonbamiOfficial@users.noreply.github.com>
2024-04-13 11:12:22 +0800

    feat: add new temporary styling to chat component

--- src/components/bot/ui/Chat.jsx
+++ src/components/bot/ui/Chat.jsx
@@ -5,6 +5,8 @@ import { FontSizeContext } from "../../../providers/FontSizeProvider";
 
 import { chatbot } from "../../../libs/bot-details";
 
+import { MdSubdirectoryArrowRight } from "react-icons/md";
+
 const Chat = ({ role, message, askingForDepts, depts, timeSent }) => {
   const [fontSize, setFontSize] = useContext(FontSizeContext);
   const [one, setOne] = useState(false);
@@ -67,50 +69,70 @@ const Chat = ({ role, message, askingForDepts, depts, timeSent }) => {
                       onClick={() => toggleOne()}
                       className="rounded-3xl px-4 py-3 text-left text-white bg-gray-500 hover:bg-gray-600 dark:bg-gray-700 dark:hover:bg-gray-800 cursor-pointer"
                     >
-                      <p className="">OFFICE OF THE CITY MAYOR (MO)</p>
+                      <p className="flex gap-1 items-center">OFFICE OF THE CITY MAYOR (MO) <MdSubdirectoryArrowRight /> </p>
                       <div
                         className={`${
                           one ? "block" : "hidden"
                         } px-4 py-3 rounded-3xl mt-2`}
                       >
-                        Service: Lorem ipsum dolor sit amet, consectetur
-                        adipiscing elit, sed do eiusmod tempor incididunt ut
-                        labore et dolore magna aliqua. Ut enim ad minim veniam,
-                        quis nostrud exercitation ullamco laboris nisi ut
-                        aliquip ex ea commodo consequat.
+                        <h3 className="font-bold text-lg mb-2">Service:</h3>
+                        <p className="">
+                          Lorem ipsum dolor sit amet, consectetur adipiscing
+                          elit, sed do eiusmod tempor incididunt ut labore et
+                          dolore magna aliqua. Ut enim ad minim veniam, quis
+                          nostrud exercitation ullamco laboris nisi ut aliquip
+                          ex ea commodo consequat.
+                        </p>
                       </div>
                     </div>
                     <div
                       onClick={() => toggleTwo()}
                       className="rounded-3xl px-4 py-3 text-left text-white bg-gray-500 hover:bg-gray-600 dark:bg-gray-700 dark:hover:bg-gray-800 cursor-pointer"
                     >
-                      <p className="">OFFICE OF THE CITY VICE MAYOR (OVM)</p>
+                      <p className="flex gap-1 items-center">OFFICE OF THE CITY VICE MAYOR (OVM) <MdSubdirectoryArrowRight /> </p>
                       <div
                         className={`${
                           two ? "block" : "hidden"
                         } px-4 py-3 rounded-3xl mt-2`}
                       >
-                        Steps: Lorem ipsum dolor sit amet, consectetur
-                        adipiscing elit, sed do eiusmod tempor incididunt ut
-                        labore et dolore magna aliqua. Blandit massa enim nec
-                        dui nunc mattis enim.
+                        <h3 className="font-bold text-lg mb-2">Service:</h3>
+                        <p className="">
+                          Lorem ipsum dolor sit amet, consectetur adipiscing
+                          elit, sed do eiusmod tempor incididunt ut labore et
+                          dolore magna aliqua. Blandit massa enim nec dui nunc
+                          mattis enim.
+                        </p>
+
+                        <h3 className="font-bold text-lg mb-2">Steps:</h3>
+                        <p className="">
+                          1. Lorem ipsum dolor sit amet. <br />
+                          2. Lorem ipsum dolor sit amet consectetur adipisicing
+                          elit. <br />
+                          3. Lorem ipsum dolor sit amet, consectetur adipisicing
+                          elit. Deserunt, at.
+                        </p>
                       </div>
                     </div>
                     <div
                       onClick={() => toggleThree()}
                       className="rounded-3xl px-4 py-3 text-left text-white bg-gray-500 hover:bg-gray-600 dark:bg-gray-700 dark:hover:bg-gray-800 cursor-pointer"
                     >
-                      <p className="">
-                        OFFICE OF THE CITY ADMINISTRATOR (ADMIN)
+                      <p className="flex gap-1 items-center">
+                        OFFICE OF THE CITY ADMINISTRATOR (ADMIN) <MdSubdirectoryArrowRight /> 
                       </p>
                       <div
                         className={`${
                           three ? "block" : "hidden"
                         } px-4 py-3 rounded-3xl mt-2`}
                       >
-                        Requirements: Lorem ipsum dolor sit amet, consectetur
-                        adipiscing elit, sed do eiusmod tempor incididunt ut
-                        labore et dolore magna aliqua.
+                        <h3 className="font-bold text-lg mb-2">Requirements:</h3>
+                        <p className="">
+                          * Lorem ipsum dolor sit amet. <br />
+                          * Lorem ipsum dolor sit amet consectetur adipisicing
+                          elit. <br />
+                          * Lorem ipsum dolor sit amet, consectetur adipisicing
+                          elit. Deserunt, at.
+                        </p>
                       </div>
                     </div>
                   </div>
