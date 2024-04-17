1b2cbc4ef86b7062ee1ce8fb96add727ffba58f5
MoonbamiOfficial <141120384+MoonbamiOfficial@users.noreply.github.com>
2024-04-17 15:28:59 +0800

    refactor: rename libs folder to lib

--- src/libs/bot-details.js
+++
@@ -1,9 +0,0 @@
-import chatbotLogo from "../assets/bacoor-mayor-vmayor.jpg";
-
-export const chatbot = {
-  logo: chatbotLogo,
-  name: "Viviane",
-  nickName: "Viv",
-  slogan: "Tanong mo, sagot ko. Dahil at home ka dito!",
-  desc: "The Chatbot for the City of Bacoor, Cavite",
-};
