d21eb9693ceab497394b65293fe6ba90a9acb20b
MoonbamiOfficial <141120384+MoonbamiOfficial@users.noreply.github.com>
2024-04-17 15:32:20 +0800

    feat: add utility to split link in bot response

---
+++ src/utils/split-link.js
@@ -0,0 +1,23 @@
+export const containsPlaceholder = (string) => {
+  const matchPlaceholderPattern = /\{=\s*[^{}]+\s*=\}/;
+  return matchPlaceholderPattern.test(string);
+};
+
+export const splitLinkToResponse = (string, intent) => {
+  const matchLinkPattern = /{=([^{}]+)=}/;
+  const link = string.match(matchLinkPattern);
+
+  if (link && intent?.startsWith("form")) {
+    const interpolatedString = string.replace(
+      matchLinkPattern,
+      `<a href="${link[1]}" class="text-blue-500 font-semibold uppercase hover:underline" target="_blank">click here to download</a>`
+    );
+    return interpolatedString;
+  } else if (link && intent?.startsWith("link")) {
+    const interpolatedString = string.replace(
+      matchLinkPattern,
+      `<a href="${link[1]}" class="text-blue-500 font-semibold uppercase hover:underline" target="_blank">click here to view</a>`
+    );
+    return interpolatedString;
+  } else return string;
+};
