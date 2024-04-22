c4d754510c7ff519bae69c02c6237485118cd7a1
MoonbamiOfficial <141120384+MoonbamiOfficial@users.noreply.github.com>
2024-04-23 00:44:07 +0800

    feat: new preview msg and dynamic timestamp to sample chat ui component

--- src/components/bot/ui/SampleBotChat.jsx
+++ src/components/bot/ui/SampleBotChat.jsx
@@ -13,13 +13,13 @@ const SampleBotChat = ({ style }) => {
         />
         <div className="max-w-[80%] rounded-3xl bg-bot dark:bg-gray-600  px-4 py-3 break-words">
           <p id="message" className="dark:text-white">
-            Lorem ipsum dolor sit.
+            This is a preview message.
           </p>
           <div
             id="timeSent"
             className="w-full text-gray-500 dark:text-gray-300 text-xs text-right"
           >
-            8:15 PM
+            {new Date().toLocaleTimeString().replace(/(.*)\D\d+/, "$1")}
           </div>
         </div>
       </div>
