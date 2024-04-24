5defbc9ca12c38f4c632ebf926605358e78b4950
MoonbamiOfficial <141120384+MoonbamiOfficial@users.noreply.github.com>
2024-04-24 09:56:56 +0800

    refactor: move firebase config to its own folder

---
+++ src/firebase/config.js
@@ -0,0 +1,30 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
+// NOTICE!: uncomment the production config and comment the dev config
+// once you're going to use our original db
+// REMINDER!: please set up the .env variables before running in dev
+
+// PRODUCTION
+const firebaseConfig = {
+  apiKey: `${import.meta.env.APIKEY}`,
+  authDomain: `${import.meta.env.AUTHDOMAIN}`,
+  projectId: "cob-chatbot",
+  storageBucket: `${import.meta.env.STORAGEBUCKET}`,
+  messagingSenderId: `${import.meta.env.MESSAGINGSENDERID}`,
+  appId: `${import.meta.env.APPID}`,
+};
+
+// DEVELOPMENT
+// const firebaseConfig = {
+//   apiKey: `${import.meta.env.APIKEYDEV}`,
+//   authDomain: `${import.meta.env.AUTHDOMAINDEV}`,
+//   projectId: "chatbot-dev-322c6",
+//   storageBucket: `${import.meta.env.STORAGEBUCKETDEV}`,
+//   messagingSenderId: `${import.meta.env.MESSAGINGSENDERIDDEV}`,
+//   appId: `${import.meta.env.APPIDDEV}`,
+//   measurementId: `${import.meta.env.MEASUREMENTIDDEV}`,
+// };
+
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
