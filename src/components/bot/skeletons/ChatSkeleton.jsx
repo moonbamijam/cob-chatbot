6d45360b46afb3fd80755de7665614480db371fc
MoonbamiOfficial <141120384+MoonbamiOfficial@users.noreply.github.com>
2024-05-08 15:59:07 +0800

    feat: update chat skeleton screen to dynamic

--- src/components/bot/skeletons/ChatSkeleton.jsx
+++ src/components/bot/skeletons/ChatSkeleton.jsx
@@ -1,40 +1,55 @@
-import { chatbot } from "../../../lib/bot-details";
+import { getDocs } from "firebase/firestore";
+import { useState, useEffect } from "react";
+import useChatbot from "../../../hooks/useChatbot";
 import SkeletonScreen from "../ui/SkeletonScreen";
 
 const ChatSkeleton = () => {
+  const { messagesQuery } = useChatbot();
+  const [skeletonizedChat, setSkeletonizedChat] = useState([]);
+
+  // get messages independently from useChatbot hook
+  const getMessagesToSkeletonized = async () => {
+    const data = await getDocs(messagesQuery);
+    setSkeletonizedChat(
+      data.docs.map((doc) => ({ ...doc.data(), id: doc.id }))
+    );
+  };
+
+  // render those skeletonized messages once
+  useEffect(() => {
+    getMessagesToSkeletonized();
+  }, []);
+
   return (
     <div className="">
-      <div className="w-full flex justify-end mt-3 mb-2">
-        <SkeletonScreen
-          id="chat"
-          width="w-[316px]"
-          height="h-[64px]"
-          customStyles="bg-primary"
-        />
-      </div>
-      <div className="w-full relative flex items-center gap-2 mt-3 mb-2">
-        <img
-          src={chatbot.logo}
-          alt="Chatbot Icon"
-          width={35}
-          height={35}
-          className="rounded-full select-none"
-        />
-        <SkeletonScreen
-          id="chat"
-          width="w-[364px]"
-          height="h-[128px]"
-          customStyles="bg-gray-300 dark:bg-gray-700 "
-        />
-      </div>
-      <div className="w-full flex justify-end mt-3 mb-2">
-        <SkeletonScreen
-          id="chat"
-          width="w-[248px]"
-          height="h-[64px]"
-          customStyles="bg-primary"
-        />
-      </div>
+      {skeletonizedChat.map((skeletonizedChat, i) => (
+        <div
+          key={i}
+          className={`${
+            skeletonizedChat.role == "bot"
+              ? "w-full relative flex items-center gap-2"
+              : "w-full flex justify-end"
+          } mt-3 mb-2`}
+        >
+          {skeletonizedChat.role == "bot" ? (
+            <div className="bg-gray-300 w-[35px] h-[35px] rounded-full select-none animate-pulse"></div>
+          ) : null}
+          <SkeletonScreen
+            id="chat"
+            width={`${
+              skeletonizedChat.role === "bot" ? "w-full" : "w-[216px]"
+            }`}
+            height={`${
+              skeletonizedChat.role === "bot" ? "h-[96px]" : "h-[64px]"
+            }`}
+            customStyles={`${
+              skeletonizedChat.role == "bot"
+                ? "bg-gray-300 dark:bg-gray-700"
+                : "bg-primary"
+            } `}
+          />
+        </div>
+      ))}
     </div>
   );
 };
