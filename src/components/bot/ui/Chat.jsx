425a248563fd635cc2df2e922292c4c671d938bf
MoonbamiOfficial <141120384+MoonbamiOfficial@users.noreply.github.com>
2024-05-08 16:01:05 +0800

    perf: improve readability and rendering to chat ui component

--- src/components/bot/ui/Chat.jsx
+++ src/components/bot/ui/Chat.jsx
@@ -10,7 +10,7 @@ import DepartmentBtn from "../buttons/DepartmentBtn";
 const step = 4;
 
 const Chat = ({ role, message, depts, timeSent, link }) => {
-  const [fontSize, setFontSize] = useContext(FontSizeContext);
+  const [fontSize] = useContext(FontSizeContext);
   const [numberOfDeptsToShow, setNumberOfDeptsToShow] = useState(step);
 
   const loadMore = () => {
@@ -32,70 +32,84 @@ const Chat = ({ role, message, depts, timeSent, link }) => {
         ));
   };
 
-  return (
-    <div className={`${role == "bot" ? "bot message" : "user message"} `}>
-      <div
-        className={`${
-          role == "bot"
-            ? "w-full relative flex items-center gap-2"
-            : "w-full flex justify-end"
-        }  mt-3 mb-2`}
-      >
-        {role == "bot" ? (
-          <img
-            src={chatbot.logo}
-            alt="Chatbot Icon"
-            width={35}
-            height={35}
-            className="rounded-full select-none"
-          />
-        ) : (
-          ""
-        )}
-        <div
-          className={`max-w-[80%] rounded-3xl ${
-            role == "bot"
-              ? "bg-bot dark:bg-gray-700"
-              : "bg-primary text-white"
-          } px-4 py-3 break-words`}
-        >
-          <div
-            id="message"
-            style={{ fontSize: fontSize }}
-            className="relative dark:text-white whitespace-pre-line"
-          >
-            {message}
-            {renderDeptsContent()}
-            {depts && (
-              <button
-                onClick={() => loadMore()}
-                className={`w-full absolute -bottom-3 bg-gradient-to-t from-bot dark:from-gray-600 from-15% h-[100px] z-[5] backdrop-blur-xs  ${
-                  depts?.length <= numberOfDeptsToShow ? "hidden" : "block"
-                } hover:backdrop-blur-0 [&>p]:hover:opacity-15`}
+  const renderChat = () => {
+    if (role === "bot") {
+      return (
+        <div className="bot message">
+          <div className="w-full relative flex items-center gap-2 mt-3 mb-2">
+            <img
+              src={chatbot.logo}
+              alt="Chatbot Icon"
+              width={35}
+              height={35}
+              className="rounded-full select-none"
+            />
+            <div className="max-w-[80%] rounded-3xl bg-bot dark:bg-gray-700 px-4 py-3 break-words">
+              <div
+                id="message"
+                style={{ fontSize: fontSize }}
+                className="relative dark:text-white whitespace-pre-line"
               >
-                <p className="font-semibold text-white drop-shadow-lg animate-bounce text-base">
-                  Click here to show more
-                </p>
-              </button>
-            )}
-            {link ? (
-              <div className="" dangerouslySetInnerHTML={{ __html: link }} />
-            ) : null}
-          </div>
-          <div
-            id="timeSent"
-            className={`w-full ${
-              role == "bot"
-                ? "text-gray-500 dark:text-gray-300"
-                : "text-gray-300"
-            } text-xs text-right opacity-80 z-[9]`}
-          >
-            {timeSent}
+                {message}
+                {renderDeptsContent()}
+                {depts && (
+                  <button
+                    onClick={() => loadMore()}
+                    className={`w-full absolute -bottom-3 bg-gradient-to-t from-bot dark:from-gray-600 from-15% h-[100px] z-[5] backdrop-blur-xs  ${
+                      depts?.length <= numberOfDeptsToShow ? "hidden" : "block"
+                    } hover:backdrop-blur-0 [&>p]:hover:opacity-15`}
+                  >
+                    <p className="font-semibold text-white drop-shadow-lg animate-bounce text-base">
+                      Click here to show more
+                    </p>
+                  </button>
+                )}
+                {link ? (
+                  <div
+                    className=""
+                    dangerouslySetInnerHTML={{ __html: link }}
+                  />
+                ) : null}
+              </div>
+              <div
+                id="timeSent"
+                className="text-gray-500 dark:text-gray-300 text-xs text-right opacity-80 z-[9]"
+              >
+                {timeSent}
+              </div>
+            </div>
           </div>
         </div>
-      </div>
-    </div>
-  );
+      );
+    } else if (role === "user") {
+      return (
+        <div className="user message">
+          <div
+            className="w-full flex justify-end
+            mt-3 mb-2"
+          >
+            <div className="max-w-[80%] rounded-3xl bg-primary text-white px-4 py-3 break-words">
+              <div
+                id="message"
+                style={{ fontSize: fontSize }}
+                className="relative dark:text-white whitespace-pre-line"
+              >
+                {message}
+              </div>
+              <div
+                id="timeSent"
+                className="w-full text-gray-300 text-xs text-right opacity-80 z-[9]"
+              >
+                {timeSent}
+              </div>
+            </div>
+          </div>
+        </div>
+      );
+    }
+  };
+
+  return <>{renderChat()}</>;
 };
 
 export default Chat;
