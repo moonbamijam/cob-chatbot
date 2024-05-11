ef26fa6e4d876dd71d0c5c9f18b3e0b2d561346b
MoonbamiOfficial <141120384+MoonbamiOfficial@users.noreply.github.com>
2024-05-11 16:45:08 +0800

    stye: format font size provider

--- src/providers/FontSizeProvider.jsx
+++ src/providers/FontSizeProvider.jsx
@@ -3,9 +3,11 @@ import React, { useEffect, useState } from "react";
 export const FontSizeContext = React.createContext();
 
 const FontSizeProvider = ({ children }) => {
-  const [fontSize, setFontSize] = useState(parseInt(localStorage.getItem("fontSize")));
+  const [fontSize, setFontSize] = useState(
+    parseInt(localStorage.getItem("fontSize"))
+  );
   // REMEMBER TO USE "parseInt" since we're always getting a string in localStorage even it is a number
-  
+
   useEffect(() => {
     if (localStorage.getItem("fontSize") == null) {
       const initialFontSize = 16;
