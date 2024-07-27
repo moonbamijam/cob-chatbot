789e79d40d81e27952991a4b1150ec2c85c94dd3
MoonbamiOfficial <141120384+MoonbamiOfficial@users.noreply.github.com>
2024-07-27 14:31:16 +0800

    feat: add mini title for settings

---
+++ src/components/bot/settings/SettingsMiniTitle.jsx
@@ -0,0 +1,9 @@
+const SettingsMiniTitle = ({ text }) => {
+  return (
+    <h3 className="capitalize text-lg font-semibold mb-2 text-black/60 dark:text-white/50">
+      {text}
+    </h3>
+  );
+};
+
+export default SettingsMiniTitle;
