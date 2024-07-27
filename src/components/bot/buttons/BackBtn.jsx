3128597b988a96b7307c064969fe41a7b8ef4533
MoonbamiOfficial <141120384+MoonbamiOfficial@users.noreply.github.com>
2024-07-27 14:38:00 +0800

    feat: new styling to bot components

--- src/components/bot/buttons/BackBtn.jsx
+++ src/components/bot/buttons/BackBtn.jsx
@@ -4,7 +4,7 @@ const BackBtn = ({ onClick, className, text }) => {
   return (
     <button
       onClick={() => onClick()}
-      className={`w-[120px] h-[50px] rounded-3xl bg-surface hover:bg-primary [&>svg>path]:hover:text-white [&>p]:hover:text-white dark:bg-dm-surface dark:hover:bg-primary text-black dark:text-white shadow-md flex justify-center items-center gap-1 ml-auto mt-5 ${className}`}
+      className={`w-[120px] h-[50px] rounded-3xl bg-surface hover:bg-primary [&>svg>path]:hover:text-white [&>p]:hover:text-white dark:bg-dm-surface dark:hover:bg-primary text-black dark:text-white shadow-md flex justify-center items-center gap-1 ${className}`}
     >
       <IoMdArrowRoundBack />
       <p className="capitalize">{text}</p>
