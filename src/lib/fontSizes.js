9c2ae582c86f555bc97c5b26a0b07b2f51e1c9c6
MoonbamiOfficial <141120384+MoonbamiOfficial@users.noreply.github.com>
2024-07-27 14:32:02 +0800

    feat: add new lists of app specific data to lib

---
+++ src/lib/fontSizes.js
@@ -0,0 +1,26 @@
+export const fontSizes = [
+  {
+    name: "small",
+    value: 8,
+  },
+  {
+    name: "medium",
+    value: 12,
+  },
+  {
+    name: "normal",
+    value: 16,
+  },
+  {
+    name: "large",
+    value: 24,
+  },
+  {
+    name: "extra large",
+    value: 32,
+  },
+  {
+    name: "2XL",
+    value: 48,
+  },
+];
