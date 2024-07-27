7e4fe1f4620b0df20af28eb289fcef6836ff0d54
MoonbamiOfficial <141120384+MoonbamiOfficial@users.noreply.github.com>
2024-07-27 14:40:03 +0800

    feat: add new ui to settings (#35)

--- src/components/bot/settings/Settings.jsx
+++ src/components/bot/settings/Settings.jsx
@@ -2,14 +2,13 @@
 import MiniProfile from "../sections/MiniProfile";
 import BackBtn from "../buttons/BackBtn";
 import ScreenDim from "../ui/ScreenDim";
-import Fonts from "./Fonts";
-import Themes from "./Themes";
 import { useState } from "react";
 import { chatbot } from "../../../lib/botDetails";
 import SettingsNavBar from "./nav/SettingsNavBar";
+import AppearanceSettings from "./appearance/AppearanceSettings";
 
-const Settings = ({ settings, toggleSettings }) => {
-  const [checked, setChecked] = useState("themes");
+const Settings = ({ settings, toggleSettings, style }) => {
+  const [checked, setChecked] = useState("appearance");
 
   const handleSettingsChange = (event) => {
     setChecked(event.target.value);
@@ -17,52 +16,43 @@ const Settings = ({ settings, toggleSettings }) => {
 
   const changeSetting = (setting) => {
     switch (setting) {
-      case "themes":
-        setChecked(setting);
-        break;
-      case "fonts":
-        setChecked(setting);
-        break;
-      case "sounds":
+      case "appearance":
         setChecked(setting);
         break;
       default:
-        setChecked("themes");
+        setChecked("appearance");
         break;
     }
   };
 
   const renderSettings = () => {
     switch (checked) {
-      case "themes":
-        return <Themes />;
-      case "fonts":
-        return <Fonts />;
-      case "sounds":
-        return <div>sup</div>;
+      case "appearance":
+        return <AppearanceSettings />;
       case chatbot.name.toLowerCase():
         return <MiniProfile state={settings} />;
       default:
-        return <Themes />;
+        return <AppearanceSettings />;
     }
   };
 
   return (
     <>
       <section
-        className={`fixed top-[5%] inset-x-0 mx-auto max-w-[60%] w-full max-h-[90%] h-full rounded-lg overflow-hidden bg-surface dark:bg-dm-surface-light flex z-[150]`}
+        className={`fixed top-[5%] inset-x-0 mx-auto max-w-[70%] w-full max-h-[90%] h-full rounded-lg overflow-hidden bg-background dark:bg-dm-surface-light flex z-[150]`}
+        style={style}
       >
         <SettingsNavBar
-          checked={checked}
+          state={checked}
           handleSettingsChange={handleSettingsChange}
           changeSetting={changeSetting}
         />
-        <div className="w-full flex flex-col px-4 py-6 dark:bg-dm-background overflow-y-scroll scrollbar-thin scrollbar-track-transparent scrollbar-thumb-surface-dark dark:scrollbar-thumb-dm-surface-dark">
+        <div className="w-full flex flex-col p-10 dark:bg-dm-background overflow-y-scroll scrollbar-thin scrollbar-track-transparent scrollbar-thumb-surface-dark dark:scrollbar-thumb-dm-surface-dark">
           <div className="">{renderSettings()}</div>
         </div>
         <BackBtn
           onClick={toggleSettings}
-          className={"absolute top-0 right-4"}
+          className={"absolute top-4 right-4"}
           text={"back"}
         />
       </section>
