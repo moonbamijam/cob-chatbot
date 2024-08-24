bb1227a0556a55e7655aad1af35111ead06b135b
Jam Moonbami <141120384+moonbamijam@users.noreply.github.com>
2024-08-24 10:35:28 +0800

    feat: new theme changer button (#45) (#58)
    # Type of Change
    
    <!-- Check what's necessary or check all if applicable. -->
    
    - [ ] BREAKING CHANGE (change that would cause existing functionality to
    not work as expected)
    - [x] Feature (change that implements a new feature)
    - [ ] Bug Fix (change that fixed a bug)
    - [x] Improvements (change that improves existing features or
    performance)
    - [ ] Documentation (change in documents)
    - [x] Chore (formatting, refactoring, styling or other operations
    commit)
    
    ###
    
    # Description
    
    <!-- Please provide at least a short description of this pr. -->
    Update the theme changer button for app UI consistency
    
    ###
    
    # Related Tickets
    
    <!-- _If this pr contains related or close an issue, please add them
    here. -->
    
    - Related Issue(s) #45 
    - Closes #
    
    ---------
    
    Co-authored-by: MoonbamiOfficial <141120384+MoonbamiOfficial@users.noreply.github.com>

---
+++ src/lib/settings/themes.js
@@ -0,0 +1,14 @@
+export const themes = [
+  {
+    name: "system",
+    value: "system",
+  },
+  {
+    name: "light",
+    value: "light",
+  },
+  {
+    name: "dark",
+    value: "dark",
+  },
+];
