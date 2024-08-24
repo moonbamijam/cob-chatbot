8f441f67fecf958be8ec4c0bdb75f32d824dd96f
MoonbamiOfficial <141120384+MoonbamiOfficial@users.noreply.github.com>
2024-08-24 10:26:01 +0800

    impr: theme changer

--- src/contexts/ThemesProvider.jsx
+++ src/contexts/ThemesProvider.jsx
@@ -2,22 +2,67 @@ import React, { useEffect, useState } from "react";
 
 export const ThemesContext = React.createContext();
 
+const ThemeMode = {
+  Light: {
+    name: "light",
+    match: window.matchMedia("prefers-color-scheme: light"),
+  },
+  Dark: {
+    name: "dark",
+    match: window.matchMedia("prefers-color-scheme: dark"),
+  },
+  System: {
+    name: "system",
+    match: window.matchMedia("prefers-color-scheme: dark"),
+  },
+};
+
 const ThemesProvider = ({ children }) => {
   const [theme, setTheme] = useState(localStorage.getItem("theme"));
 
   useEffect(() => {
-    if (theme === "dark") {
-      window.matchMedia("prefer-color-scheme: dark");
-      localStorage.setItem("theme", "dark");
-    } else {
-      window.matchMedia("prefer-color-scheme: light");
-      localStorage.setItem("theme", "light");
+    switch (theme) {
+      case ThemeMode.Light.name:
+        localStorage.setItem("theme", ThemeMode.Light.name);
+        document.documentElement.setAttribute("theme", ThemeMode.Light.name);
+        document.documentElement.classList.add(ThemeMode.Light.name);
+        document.documentElement.classList.remove(
+          ThemeMode.Dark.name,
+          ThemeMode.System.name,
+        );
+        ThemeMode.Light.match;
+        break;
+      case ThemeMode.Dark.name:
+        localStorage.setItem("theme", ThemeMode.Dark.name);
+        document.documentElement.setAttribute("theme", ThemeMode.Dark.name);
+        document.documentElement.classList.add(ThemeMode.Dark.name);
+        document.documentElement.classList.remove(
+          ThemeMode.Light.name,
+          ThemeMode.System.name,
+        );
+        ThemeMode.Dark.match;
+        break;
+      case ThemeMode.System.name:
+        localStorage.setItem("theme", ThemeMode.System.name);
+        document.documentElement.setAttribute("theme", ThemeMode.System.name);
+        document.documentElement.classList.add(ThemeMode.Dark.name);
+        document.documentElement.classList.remove(
+          ThemeMode.Light.name,
+          ThemeMode.System.name,
+        );
+        ThemeMode.System.match;
+        break;
+      default:
+        localStorage.setItem("theme", ThemeMode.Light.name);
+        document.documentElement.setAttribute("theme", ThemeMode.Light.name);
+        document.documentElement.classList.add(ThemeMode.Light.name);
+        document.documentElement.classList.remove(
+          ThemeMode.Dark.name,
+          ThemeMode.System.name,
+        );
+        ThemeMode.Light.match;
+        break;
     }
-  }, [theme]);
-
-  useEffect(() => {
-    if (theme === "dark") document.documentElement.classList.add("dark");
-    else document.documentElement.classList.remove("dark");
   }, [theme]);
 
   return (
