a6039f0f939aa4a93ed98b0efd203fc58f0c3716
MoonbamiOfficial <141120384+MoonbamiOfficial@users.noreply.github.com>
2024-08-28 07:09:59 +0800

    feat: hide large screen btn on small screens

--- src/components/bot/buttons/MaximizeBtn.jsx
+++ src/components/bot/buttons/MaximizeBtn.jsx
@@ -4,7 +4,7 @@ const MaximizeBtn = ({ state, onClick }) => {
   return (
     <button
       onClick={() => onClick()}
-      className="rounded-full p-4 hover:bg-primary [&>svg>path]:hover:text-white [&>svg>path]:dark:text-white"
+      className="hidden xl:block rounded-full p-4 hover:bg-primary [&>svg>path]:hover:text-white [&>svg>path]:dark:text-white"
     >
       {state ? <LuMinimize /> : <LuMaximize />}
     </button>
