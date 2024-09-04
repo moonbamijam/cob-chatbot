5587165a58252b03323720ae8d2a8dcfd8211c4a
MoonbamiOfficial <141120384+MoonbamiOfficial@users.noreply.github.com>
2024-09-04 16:39:10 +0800

    feat: new styling to faqs message

--- src/components/bot/buttons/SuggestedMessageBtn.jsx
+++ src/components/bot/buttons/SuggestedMessageBtn.jsx
@@ -2,9 +2,7 @@ const SuggestedMessageBtn = ({ message, onClick, className, isFaq }) => {
   return (
     <button
       onClick={onClick}
-      className={`${
-        isFaq && "w-[400px]"
-      } rounded-3xl border border-primary text-primary dark:text-white px-4 py-3 hover:bg-primary hover:text-white active:translate-y-1 ${className}`}
+      className={`rounded-3xl border border-primary text-xs xs:text-sm sm:text-base text-primary dark:text-white px-4 py-3 hover:bg-primary hover:text-white active:translate-y-1 ${className}`}
     >
       {message}
     </button>
