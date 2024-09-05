9cf569867259305620fca464f984c1aafa46dd28
Jam Moonbami <141120384+moonbamijam@users.noreply.github.com>
2024-09-05 14:33:11 +0800

    feat: add pwa support (#80)
    # Type of Change
    
    <!-- Check what's necessary or check all if applicable. -->
    
    - [ ] BREAKING CHANGE (change that would cause existing functionality to
    not work as expected)
    - [x] Feature (change that implements a new feature)
    - [ ] Bug Fix (change that fixed a bug)
    - [ ] Improvements (change that improves existing features or
    performance)
    - [ ] Documentation (change in documents)
    - [ ] Chore (formatting, refactoring, styling or other operations
    commit)
    
    ###
    
    # Description
    
    <!-- Please provide at least a short description of this pr. -->
    Add PWA support using vite-pwa-plugin package
    
    ###
    
    # Related Tickets
    
    <!-- _If this pr contains related or close an issue, please add them
    here. -->
    
    - Related Issue(s) #
    - Closes #
    
    Co-authored-by: MoonbamiOfficial <141120384+MoonbamiOfficial@users.noreply.github.com>

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,15 @@
 import { defineConfig } from "vite";
+import { VitePWA } from "vite-plugin-pwa";
 import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: "autoUpdate",
+      manifestFilename: "site.webmanifest",
+      manifest: false,
+    }),
+  ],
 });
