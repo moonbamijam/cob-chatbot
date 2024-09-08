c29e2ed848f8464abcff9bc0b4acbe3b02abdff6
MoonbamiOfficial <141120384+MoonbamiOfficial@users.noreply.github.com>
2024-09-08 22:41:01 +0800

    fix: api url exposure

---
+++ src/lib/bot/chatbotConfig.js
@@ -0,0 +1,12 @@
+import chatbotLogo from "../../assets/images/bacoor-mayor-vmayor.jpg";
+
+export const chatbotConfig = {
+  url: import.meta.env.VITE_CHATBOT_API_PROD,
+  logo: chatbotLogo,
+  name: "Viviane",
+  nickName: "Viviane",
+  slogan: "Tanong mo, sagot ko. Dahil at home ka dito!",
+  desc: "Introducing Viviane: The Interactive Chatbot for the City of Bacoor, Cavite.",
+  initialGreet:
+    "👋 Hello there! My name is Viviane, your friendly chatbot assistant. Welcome to our conversation! Whether you're here for assistance or information I'm here to help. Feel free to ask me anything or simply say hi.",
+};
