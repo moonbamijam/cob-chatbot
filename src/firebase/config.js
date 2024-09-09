92ff1aa0da98fbff557520ea7ada9ad2f09a4a7b
MoonbamiOfficial <141120384+MoonbamiOfficial@users.noreply.github.com>
2024-09-09 13:30:18 +0800

    chore: update firebase config and env var names

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,42 +1,17 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
-// NOTICE!: replace the argument in the getFirebaseConfig call
-// to use the corresponding database configuration
-// REMINDER!: please set up the .env variables before running in dev
+// Please set up the env variables
 
-const getFirebaseConfig = (projectMode) => {
-  let config = {};
-  // PRODUCTION
-  if (projectMode.toLowerCase() === "production") {
-    config = {
-      apiKey: `${import.meta.env.VITE_APIKEY}`,
-      authDomain: `${import.meta.env.AUTHDOMAIN}`,
-      projectId: "cob-chatbot",
-      storageBucket: `${import.meta.env.STORAGEBUCKET}`,
-      messagingSenderId: `${import.meta.env.MESSAGINGSENDERID}`,
-      appId: `${import.meta.env.APPID}`,
-    };
-  }
-  // DEVELOPMENT
-  if (projectMode.toLowerCase() === "development") {
-    config = {
-      apiKey: `${import.meta.env.VITE_APIKEYDEV}`,
-      authDomain: `${import.meta.env.AUTHDOMAINDEV}`,
-      projectId: "chatbot-dev-322c6",
-      storageBucket: `${import.meta.env.STORAGEBUCKETDEV}`,
-      messagingSenderId: `${import.meta.env.MESSAGINGSENDERIDDEV}`,
-      appId: `${import.meta.env.APPIDDEV}`,
-      measurementId: `${import.meta.env.MEASUREMENTIDDEV}`,
-    };
-  }
-  if (!["production", "development"].includes(projectMode.toLowerCase())) {
-    console.error("Invalid projectMode:", projectMode);
-  }
-  return config;
+const firebaseConfig = {
+  apiKey: import.meta.env.VITE_API_KEY,
+  authDomain: import.meta.env.AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_PROJECT_ID,
+  storageBucket: import.meta.env.STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.MESSAGING_SENDER_ID,
+  appId: import.meta.env.APP_ID,
+  measurementId: import.meta.env.MEASUREMENT_ID,
 };
-// Modify the argument accordingly
-const firebaseConfig = getFirebaseConfig("development");
 
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
