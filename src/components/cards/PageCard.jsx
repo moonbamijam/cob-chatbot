375a16800cb07714d41db2e40d76cf54d9289fb0
MoonbamiOfficial <141120384+MoonbamiOfficial@users.noreply.github.com>
2024-09-11 22:05:12 +0800

    feat: add responsiveness to mobile screens to modal

--- src/components/cards/PageCard.jsx
+++ src/components/cards/PageCard.jsx
@@ -1,8 +1,5 @@
 import { Link } from "react-router-dom";
 
-// components
-import Card from "../ui/Card";
-
 // assets
 import CityHallCloseUp from "../../../static/assets/images/default-page-card.jpg";
 
@@ -10,36 +7,32 @@ const PageCard = ({ img, name, comingSoon }) => {
   const renderPageCard = () => {
     if (comingSoon) {
       return (
-        <Card>
-          <button className="relative max-w-[400px] max-h-[150px] flex justify-center items-center bg-dm-surface-dark rounded-lg overflow-hidden group">
-            <img
-              src={CityHallCloseUp}
-              alt=""
-              className="w-[400px] h-[150px] object-cover opacity-20 group-hover:opacity-30"
-            />
-            <h3 className="absolute opacity-100 text-white text-3xl font-helvetica-rounded-bold">
-              Coming soon
-            </h3>
-          </button>
-        </Card>
+        <button className="relative w-full max-w-[400px] h-full max-h-[150px] flex justify-center items-center bg-dm-surface-dark rounded-lg overflow-hidden group">
+          <img
+            src={CityHallCloseUp}
+            alt=""
+            className="w-[400px] h-[150px] object-cover opacity-20 group-hover:opacity-30"
+          />
+          <h3 className="absolute opacity-100 text-white text-3xl font-helvetica-rounded-bold">
+            Coming soon
+          </h3>
+        </button>
       );
     } else {
       return (
-        <Card>
-          <Link
-            to="/bacoor-gov"
-            className="relative max-w-[400px] max-h-[150px] flex justify-center items-center bg-dm-surface-dark rounded-lg overflow-hidden group"
-          >
-            <img
-              src={img}
-              alt=""
-              className="w-[400px] h-[150px] object-cover opacity-50 dark:opacity-40 group-hover:opacity-90"
-            />
-            <h3 className="absolute opacity-100 group-hover:opacity-0 text-white text-3xl capitalize font-helvetica-rounded-bold">
-              {name}
-            </h3>
-          </Link>
-        </Card>
+        <Link
+          to="/bacoor-gov"
+          className="relative w-full max-w-[400px] h-full max-h-[150px] flex justify-center items-center bg-dm-surface-dark rounded-lg overflow-hidden group"
+        >
+          <img
+            src={img}
+            alt=""
+            className="w-[400px] h-[150px] object-cover opacity-50 dark:opacity-40 group-hover:opacity-90"
+          />
+          <h3 className="absolute opacity-100 group-hover:opacity-0 text-white text-3xl capitalize font-helvetica-rounded-bold">
+            {name}
+          </h3>
+        </Link>
       );
     }
   };
