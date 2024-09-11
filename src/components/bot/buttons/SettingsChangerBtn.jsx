8b1e401b6053b17bb90b8fad10441c8092e09536
MoonbamiOfficial <141120384+MoonbamiOfficial@users.noreply.github.com>
2024-09-11 15:13:12 +0800

    feat: few ui adjustments to settings

--- src/components/bot/buttons/SettingsChangerBtn.jsx
+++ src/components/bot/buttons/SettingsChangerBtn.jsx
@@ -10,20 +10,20 @@ const SettingsChangerBtn = ({
 }) => {
   return (
     <button onClick={onClick} className="active:translate-y-1">
-      <input
-        type="radio"
-        name={name}
-        id={id}
-        value={value}
-        checked={checkedIf}
-        onChange={onChange}
-        className="hidden peer"
-      />
       <label
         htmlFor={id}
-        className={`bg-surface hover:bg-primary hover:text-white dark:hover:bg-primary dark:bg-dm-surface dark:text-white peer-checked:bg-primary peer-checked:hover:bg-primary-dark peer-checked:text-white shadow-md w-[120px] h-[50px] px-4 py-3 flex justify-center items-center rounded-3xl capitalize cursor-pointer truncate`}
+        className={`bg-surface has-[input]:hover:bg-primary has-[input]:hover:text-white has-[input]:dark:hover:bg-primary dark:bg-dm-surface dark:text-white has-[:checked]:bg-primary has-[:checked]:dark:bg-primary has-[:checked]:hover:bg-primary-dark has-[:checked]:text-white shadow-md w-[120px] h-[50px] px-4 py-3 flex justify-center items-center rounded-3xl capitalize cursor-pointer truncate`}
         style={style}
       >
+        <input
+          type="radio"
+          name={name}
+          id={id}
+          value={value}
+          checked={checkedIf}
+          onChange={onChange}
+          className="hidden"
+        />
         {displayedText}
       </label>
     </button>
--- src/components/bot/settings/nav/SettingsNavTitle.jsx
+++ src/components/bot/settings/nav/SettingsNavTitle.jsx
@@ -1,6 +1,6 @@
 const SettingsNavTitle = ({ name }) => {
   return (
-    <p className="w-full text-xs text-black/60 dark:text-white/50 font-semibold capitalize md:px-4 line-clamp-2 break-words">
+    <p className="w-full text-xs text-black/60 dark:text-white/50 font-helvetica-bold capitalize md:px-4 line-clamp-2 break-words">
       {name}
     </p>
   );
