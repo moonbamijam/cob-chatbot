ae83770d3ad5f862c336d3f7a4139e2833b6cbd0
Jam Moonbami <141120384+moonbamijam@users.noreply.github.com>
2024-09-13 21:06:47 +0800

    feat: seo and assets update (#115)
    # Type of Change
    
    <!-- Check what's necessary or check all if applicable. -->
    
    - [ ] BREAKING CHANGE (change that would cause existing functionality to
    not work as expected)
    - [x] Feature (change that implements a new feature)
    - [ ] Bug Fix (change that fixed a bug)
    - [x] Improvements (change that improves existing features or
    performance)
    - [ ] Documentation (change in documents)
    - [ ] Chore (formatting, refactoring, styling or other operations
    commit)
    
    ###
    
    # Description
    
    <!-- Please provide at least a short description of this pr. -->
    Update assets and finalize chatbot's name
    
    ###
    
    # Related Tickets
    
    <!-- _If this pr contains related or close an issue, please add them
    here. -->
    
    - Related Issue(s) #
    - Closes #
    
    ---------
    
    Co-authored-by: MoonbamiOfficial <141120384+MoonbamiOfficial@users.noreply.github.com>

--- src/lib/bot/chatbotConfig.js
+++ src/lib/bot/chatbotConfig.js
@@ -1,15 +1,15 @@
-import chatbotLogo from "../../assets/images/bacoor-mayor-vmayor.jpg";
+import chatbotLogo from "../../../static/assets/images/logo.png";
 
 export const chatbotConfig = {
   url: import.meta.env.VITE_CHATBOT_API_URL,
   logo: chatbotLogo,
-  name: "Viviane",
-  nickName: "Viviane",
-  slogan: "Tanong mo, sagot ko. Dahil at home ka dito!",
-  desc: "Introducing Bacoor, Cavite's interactive chatbot. Viviane, a chatbot tasked with responding to inquiries about Bacoor.",
+  name: "Vivienne",
+  nickName: "Vivienne",
+  slogan: "Tanong mo, sagot ko, dahil at home ka dito!",
+  desc: "Introducing Bacoor, Cavite's interactive chatbot. Vivienne, a chatbot tasked with responding to inquiries about Bacoor.",
   introduction: "The interactive chatbot for the city of Bacoor, Cavite.",
   initialGreet:
-    "👋 Hello there! My name is Viviane, your friendly chatbot assistant. Welcome to our conversation! Whether you're here for assistance or information I'm here to help. Feel free to ask me anything or simply say hi.",
+    "👋 Hello there! My name is Vivienne, your friendly chatbot assistant. Welcome to our conversation! Whether you're here for assistance or information I'm here to help. Feel free to ask me anything or simply say hi.",
   errorMessage:
     "Sorry, I can't seem to understand what your saying. Could you please try it again?",
 };
