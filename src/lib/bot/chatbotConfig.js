a01f9b98ee42f045d53f41a91ab8666b7f0753a2
MoonbamiOfficial <141120384+MoonbamiOfficial@users.noreply.github.com>
2024-09-13 20:49:22 +0800

    refactor: finalize chatbot name

--- src/lib/bot/chatbotConfig.js
+++ src/lib/bot/chatbotConfig.js
@@ -3,13 +3,13 @@ import chatbotLogo from "../../assets/images/bacoor-mayor-vmayor.jpg";
 export const chatbotConfig = {
   url: import.meta.env.VITE_CHATBOT_API_URL,
   logo: chatbotLogo,
-  name: "Libra",
-  nickName: "Libra",
+  name: "Vivienne",
+  nickName: "Vivienne",
   slogan: "Tanong mo, sagot ko, dahil at home ka dito!",
-  desc: "Introducing Bacoor, Cavite's interactive chatbot. Libra, a chatbot tasked with responding to inquiries about Bacoor.",
+  desc: "Introducing Bacoor, Cavite's interactive chatbot. Vivienne, a chatbot tasked with responding to inquiries about Bacoor.",
   introduction: "The interactive chatbot for the city of Bacoor, Cavite.",
   initialGreet:
-    "👋 Hello there! My name is Libra, your friendly chatbot assistant. Welcome to our conversation! Whether you're here for assistance or information I'm here to help. Feel free to ask me anything or simply say hi.",
+    "👋 Hello there! My name is Vivienne, your friendly chatbot assistant. Welcome to our conversation! Whether you're here for assistance or information I'm here to help. Feel free to ask me anything or simply say hi.",
   errorMessage:
     "Sorry, I can't seem to understand what your saying. Could you please try it again?",
 };
