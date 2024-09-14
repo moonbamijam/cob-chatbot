f9e0858206425e89019f1e89f278277acc9c7c9a
MoonbamiOfficial <141120384+MoonbamiOfficial@users.noreply.github.com>
2024-09-14 14:10:07 +0800

    feat: increase size of button ui component

--- src/components/ui/Button.jsx
+++ src/components/ui/Button.jsx
@@ -8,7 +8,7 @@ const buttonVariants = cva("w-full h-full rounded-full", {
     variant: {
       default:
         "border dark:border-dm-surface-light bg-surface dark:bg-dm-surface",
-      icon: "text-2xl rounded-full p-2 flex justify-center items-center",
+      icon: "text-2xl rounded-full p-3 flex justify-center items-center",
       outline: "px-4 py-3 border border-primary",
       cta: "px-8 py-4 capitalize truncate",
       destructive: "px-8 py-4 bg-error hover:bg-error/80 capitalize",
@@ -21,7 +21,7 @@ const buttonVariants = cva("w-full h-full rounded-full", {
       md: "max-w-[100px] max-h-[40px]",
       lg: "max-w-[150px] max-h-[60px]",
       xl: "max-w-[200px] max-h-[80px]",
-      icon: "max-w-[40px] max-h-[40px]",
+      icon: "max-w-[50px] max-h-[50px]",
     },
   },
   defaultVariants: {
