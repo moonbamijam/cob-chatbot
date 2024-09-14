3eceeb2e7beda6839bea681ea1fda1698ae9a5ad
MoonbamiOfficial <141120384+MoonbamiOfficial@users.noreply.github.com>
2024-09-14 16:03:12 +0800

    feat: re ui settings nav

--- src/components/bot/settings/SettingsBtn.jsx
+++ src/components/bot/settings/SettingsBtn.jsx
@@ -1,32 +1,30 @@
+import { Button, buttonVariants } from "../../ui/Button";
+
 const SettingsBtn = ({ name, checkedIf, onClick, onChange, icon, state }) => {
   return (
-    <button
-      onClick={() => onClick()}
-      className={`${state ? "w-full" : "w-max sm:w-full "} flex rounded-lg text-left font-helvetica-bold hover:bg-gray-300 dark:hover:bg-dm-surface dark:text-white`}
+    <label
+      htmlFor={name}
+      className={`${state ? "w-full" : "w-max md:w-full "} ${buttonVariants({ variant: "settings", size: "full" })}`}
+      onClick={onClick}
     >
-      <label
-        htmlFor={name}
-        className={`${state ? "w-full" : "w-max sm:w-full "} capitalize rounded-lg px-4 py-3 text-black/50 dark:text-white/60 has-[:checked]:dark:text-white has-[:checked]:text-white has-[:checked]:bg-primary has-[:checked]:hover:bg-primary-dark cursor-pointer truncate`}
-      >
-        <input
-          type="radio"
-          name="settings"
-          id={name}
-          value={name}
-          checked={checkedIf}
-          onChange={onChange}
-          className="hidden"
-        />
-        {state ? (
-          <span className="md:block">{name}</span>
-        ) : (
-          <>
-            <span className="hidden md:block">{name}</span>
-            <div className="text-xl md:hidden">{icon}</div>
-          </>
-        )}
-      </label>
-    </button>
+      <input
+        type="radio"
+        name="settings"
+        id={name}
+        value={name}
+        checked={checkedIf}
+        onChange={onChange}
+        className="hidden"
+      />
+      {state ? (
+        <span className="md:block">{name}</span>
+      ) : (
+        <>
+          <span className="hidden md:block">{name}</span>
+          <div className="text-xl p-0 md:hidden">{icon}</div>
+        </>
+      )}
+    </label>
   );
 };
 
--- src/components/bot/settings/nav/SettingsNavBar.jsx
+++ src/components/bot/settings/nav/SettingsNavBar.jsx
@@ -1,30 +1,47 @@
-import { useState } from "react";
-import { LuMenu } from "react-icons/lu";
-
+import { useState, useEffect, useRef } from "react";
 import { settingsList } from "../../../../lib/settings/settingsList";
+
+// components
+import { Button } from "../../../ui/Button";
 import SettingsBtn from "../SettingsBtn";
 import SettingsNavSection from "./SettingsNavSection";
 import SettingsNavTitle from "./SettingsNavTitle";
-
 import Version from "../../ui/Version";
-import Button from "../../ui/Button";
+
+// icons
+import { LuMenu } from "react-icons/lu";
 
 const SettingsNavBar = ({ state, handleSettingsChange, changeSetting }) => {
   const [isSideNavActive, setIsSideNavActive] = useState(false);
+  const sideNavRef = useRef(null);
 
   const toggleSideNav = () => {
     setIsSideNavActive(!isSideNavActive);
   };
 
+  useEffect(() => {
+    const handleSideNav = (event) => {
+      console.log("running");
+      if (!sideNavRef.current?.contains(event.target))
+        setIsSideNavActive(false);
+    };
+    document.addEventListener("mousedown", handleSideNav);
+    return () => {
+      document.removeEventListener("mousedown", handleSideNav);
+    };
+  }, []);
+
   return (
     <>
       <nav
-        className={`absolute top-0 left-0 bottom-0 h-full px-4 py-10 flex flex-col items-start sm:justify-between bg-surface dark:bg-dm-surface-dark ${isSideNavActive ? "w-[50%] md:w-[20%]" : "w-[20%]"}`}
+        className={`absolute top-0 left-0 bottom-0 h-full px-4 py-10 flex flex-col items-start sm:justify-between bg-surface dark:bg-dm-surface-dark ${isSideNavActive ? "w-[50%] md:w-[20%]" : "w-[20%] sm:w-[15%] md:w-[20%]"} z-50`}
+        ref={sideNavRef}
       >
         <Button
-          state={isSideNavActive}
+          variant="icon"
+          size="icon"
           onClick={toggleSideNav}
-          className="w-max md:hidden mb-10"
+          className={`${isSideNavActive ? "bg-primary [&>svg>line]:text-white" : "hover:bg-surface-dark dark:hover:bg-dm-surface"} [&>svg>line]:dark:text-white md:hidden mb-10`}
         >
           <LuMenu />
         </Button>
