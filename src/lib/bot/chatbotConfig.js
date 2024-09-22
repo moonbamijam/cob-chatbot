d1f0c1f9ff98b08c885b1e7b737664a4c10d2b3b
MoonbamiOfficial <141120384+MoonbamiOfficial@users.noreply.github.com>
2024-09-22 23:00:10 +0800

    feat: add dynamic chatbot configurations

--- src/lib/bot/chatbotConfig.js
+++ src/lib/bot/chatbotConfig.js
@@ -1,15 +1,34 @@
-import chatbotLogo from "../../../static/assets/images/logo.png";
+// db
+import { collection, doc, onSnapshot } from "firebase/firestore";
+import { db } from "../../firebase/config";
 
-export const chatbotConfig = {
-  url: import.meta.env.VITE_CHATBOT_API_URL,
-  logo: chatbotLogo,
-  name: "Vivienne",
-  nickName: "Vivienne",
-  slogan: "Tanong mo, sagot ko, dahil at home ka dito!",
-  desc: "Introducing Bacoor, Cavite's interactive chatbot. Vivienne, a chatbot tasked with responding to inquiries about Bacoor.",
-  introduction: "The interactive chatbot for the city of Bacoor, Cavite.",
-  initialGreet:
-    "👋 Hello there! My name is Vivienne, your friendly chatbot assistant. Welcome to our conversation! Whether you're here for assistance or information I'm here to help. Feel free to ask me anything or simply say hi.",
-  errorMessage:
-    "Sorry, I can't seem to understand what your saying. Could you please try it again?",
+const botProfileCollectionRef = collection(db, "profile");
+
+const getBotProfile = async () => {
+  console.log("Getting bot information...");
+  // set the default values of the chatbot
+  let chatbotConfig = {
+    url: import.meta.env.VITE_CHATBOT_API_URL,
+    logo: "",
+    name: "",
+    slogan: "",
+    introduction: "The interactive chatbot for the city of Bacoor, Cavite.",
+    initialGreet: "",
+    errorMessage:
+      "Sorry, I can't seem to understand what your saying. Could you please try it again?",
+  };
+  try {
+    onSnapshot(doc(botProfileCollectionRef, "botProfile"), (doc) => {
+      chatbotConfig.logo = doc.data().interactionIconURL;
+      chatbotConfig.name = doc.data().botName;
+      chatbotConfig.slogan = doc.data().metadata.slogan;
+      chatbotConfig.initialGreet = `👋 Hello there! My name is ${doc.data().botName}, your friendly chatbot assistant. Welcome to our conversation! Whether you're here for assistance or information I'm here to help. Feel free to ask me anything or simply say hi.`;
+    });
+    console.log("Bot information received.");
+    return chatbotConfig;
+  } catch (error) {
+    console.log(error);
+  }
 };
+
+export const chatbotConfig = await getBotProfile();
