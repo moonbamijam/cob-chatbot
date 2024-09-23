a7269b27038bbadda4d79c489dcb26a910de9fa2
MoonbamiOfficial <141120384+MoonbamiOfficial@users.noreply.github.com>
2024-09-23 15:39:38 +0800

    feat: update greet

--- src/utils/greet.js
+++ src/utils/greet.js
@@ -1,25 +1,23 @@
-import { Timestamp, collection, doc, getDoc, setDoc } from "firebase/firestore";
-import { chatbotConfig } from "../lib/bot/chatbotConfig";
+import { Timestamp, collection, doc, setDoc } from "firebase/firestore";
 import { db } from "../firebase/config";
 import { v4 as uuid } from "uuid";
 
 const usersCollectionRef = collection(db, "users");
 
-export const greet = async (uid) => {
-  const botMessageInfo = {
-    intent: "greetings",
-    message: chatbotConfig.initialGreet,
-    messageId: uuid(),
-    role: "bot",
-    timeSent: Timestamp.now(),
-  };
+export const greet = async (uid, message) => {
+  try {
+    const botMessageInfo = {
+      intent: "greetings",
+      message: message,
+      messageId: uuid(),
+      role: "bot",
+      timeSent: Timestamp.now(),
+    };
 
-  const res = doc(usersCollectionRef, uid);
-  const data = await getDoc(res);
-  // this if check will avoid replacing all messages in the conversation everytime the page reloads
-  if (!data.exists()) {
     await setDoc(doc(usersCollectionRef, uid), {
       conversation: [botMessageInfo],
     });
+  } catch (error) {
+    console.log(error);
   }
 };
