b6c8429032c1288d2a8f5b955c84641d8109e570
MoonbamiOfficial <141120384+MoonbamiOfficial@users.noreply.github.com>
2024-09-29 12:06:10 +0800

    feat: add chat head size list

---
+++ src/lib/settings/chatHeadSizeList.js
@@ -0,0 +1,14 @@
+export const chatHeadSizeList = [
+  {
+    name: "small",
+    value: 100,
+  },
+  {
+    name: "medium",
+    value: 200,
+  },
+  {
+    name: "large",
+    value: 300,
+  },
+];
