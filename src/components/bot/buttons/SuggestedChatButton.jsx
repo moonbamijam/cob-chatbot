2d019397dc58d50984d044d206da2b6231e54d91
MoonbamiOfficial <141120384+MoonbamiOfficial@users.noreply.github.com>
2024-10-02 16:17:29 +0800

    feat: small size increase to suggested chat btn

--- src/components/bot/buttons/SuggestedChatButton.jsx
+++ src/components/bot/buttons/SuggestedChatButton.jsx
@@ -4,8 +4,8 @@ const SuggestedChatButton = ({ children, ...props }) => {
   return (
     <Button
       variant="outline"
-      size="lg"
-      className="rounded-3xl border border-primary text-xs xs:text-sm text-primary dark:text-white hover:bg-primary hover:text-white active:translate-y-1"
+      size="xl"
+      className="rounded-3xl w-full h-full border border-primary text-xs xs:text-sm text-primary dark:text-white hover:bg-primary hover:text-white  active:translate-y-1"
       {...props}
     >
       {children}
