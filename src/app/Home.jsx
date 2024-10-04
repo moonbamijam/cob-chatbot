aa803e4fb0db42a0b3c7ab2a3db82902bb9dfbb2
Jam Moonbami <141120384+moonbamijam@users.noreply.github.com>
2024-10-04 19:47:33 +0800

    perf: new cta button to landing page (#136)
    * feat: new cta button to landing page
    
    refactor: remove modal
    
    * style: remove unused imports
    
    ---------
    
    Co-authored-by: MoonbamiOfficial <141120384+MoonbamiOfficial@users.noreply.github.com>

--- src/app/Home.jsx
+++ src/app/Home.jsx
@@ -1,40 +1,18 @@
-import { useState, useEffect, useRef } from "react";
 import { chatbotConfig } from "../lib/bot/chatbotConfig";
 
 // components
 import { Button } from "../components/ui/Button";
-import ScreenDim from "../components/bot/ui/ScreenDim";
-import Grid from "../components/common/Grid";
-import PageCard from "../components/cards/PageCard";
 import Header from "../components/header/Header";
 
 // assets
 import LandingCoverLight from "../../static/assets/gif/landing-convo-light.gif";
 import LandingCoverDark from "../../static/assets/gif/landing-convo-dark.gif";
-import Cityhall from "../../static/assets/images/city-hall.jpg";
 
 // icons
-import { CgClose } from "react-icons/cg";
 import Version from "../components/bot/ui/Version";
+import { Link } from "react-router-dom";
 
 const Home = () => {
-  const [isModalActive, setIsModalActive] = useState(false);
-  const modalRef = useRef();
-
-  const toggleModal = () => {
-    setIsModalActive(!isModalActive);
-  };
-
-  useEffect(() => {
-    const handleModal = (event) => {
-      if (!modalRef.current?.contains(event.target)) setIsModalActive(false);
-    };
-    document.addEventListener("mousedown", handleModal);
-    return () => {
-      document.removeEventListener("mousedown", handleModal);
-    };
-  }, [isModalActive]);
-
   return (
     <>
       <Header />
@@ -51,14 +29,15 @@ const Home = () => {
           <p className="text-xl text-black/50 dark:text-white/50 ml-2 mt-6 max-w-[350px]">
             {chatbotConfig.introduction}
           </p>
-          <Button
-            onClick={toggleModal}
-            variant="cta"
-            size="xl"
-            className="h-max mt-8 rounded-[64px] ml-2 bg-primary hover:bg-primary-dark lg:text-lg 2xl:text-xl text-white capitalize"
-          >
-            ask {chatbotConfig.name}
-          </Button>
+          <Link to="/bacoor-gov">
+            <Button
+              variant="cta"
+              size="xl"
+              className="mt-8 ml-2 bg-primary hover:bg-primary-dark lg:text-lg 2xl:text-xl text-white"
+            >
+              ask {chatbotConfig.name}
+            </Button>
+          </Link>
         </section>
         <div className="pb-2 lg:border border-surface dark:border-dm-surface rounded-2xl overflow-hidden select-none">
           <img
@@ -81,42 +60,6 @@ const Home = () => {
           className="fixed left-[2%] bottom-[2.5%] text-sm text-black/50 dark:text-white/50"
         />
       </main>
-      <section
-        ref={modalRef}
-        className={`${isModalActive ? "animate-open-modal opacity-100 visible" : "opacity-0 invisible"} fixed top-[50%] left-[50%] -translate-x-[50%] -translate-y-[50%] w-[90%] max-w-[1200px] h-full max-h-[650px] sm:max-h-[700px] lg:max-h-[750px] xl:max-h-[800px] flex flex-col bg-surface dark:bg-dm-surface z-40 px-8 py-10 xl:py-20 xl:px-16 rounded-xl`}
-      >
-        <Button
-          onClick={toggleModal}
-          variant="icon"
-          size="icon"
-          className="sm:hidden border ml-auto text-error border-error hover:bg-error [&>svg>path]:hover:text-white mb-6"
-        >
-          <CgClose />
-        </Button>
-        <div className="flex justify-between items-center">
-          <div className="text-xl sm:text-2xl md:text-3xl xl:text-4xl w-max dark:text-white">
-            <h1>Hello Bacooreños!</h1>
-            <h2 className="opacity-70">You can ask me about,</h2>
-          </div>
-          <Button
-            onClick={toggleModal}
-            variant="destructiveOutline"
-            size="lg"
-            className={`hidden text-xl sm:block hover:text-white ${!isModalActive && "pointer-events-none"}`}
-          >
-            back
-          </Button>
-        </div>
-        <Grid className="lg:grid-cols-3 sm:justify-center gap-8 mt-8 xs:mt-10 xl:mt-16">
-          <PageCard name="Bacoor city hall" img={Cityhall} />
-          <PageCard comingSoon />
-        </Grid>
-      </section>
-      <ScreenDim
-        className={`bg-black z-30 backdrop-blur ${
-          isModalActive ? "opacity-80" : "opacity-0 invisible"
-        }`}
-      />
     </>
   );
 };
