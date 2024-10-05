791d971d0b8be88f47ed28eff15ba0f4491cfdf9
MoonbamiOfficial <141120384+MoonbamiOfficial@users.noreply.github.com>
2024-10-05 12:56:16 +0800

    feat: small ui change to about

--- src/components/bot/sections/MiniProfile.jsx
+++ src/components/bot/sections/MiniProfile.jsx
@@ -1,6 +1,10 @@
 import { Link } from "react-router-dom";
 import { useContext } from "react";
 import { ChatbotContext } from "../../../contexts/ChatbotContext";
+import { chatbotConfig } from "../../../lib/bot/chatbotConfig";
+import { Image } from "../../ui/Image";
+import DevIcon from "../icons/DevIcon";
+import { Button } from "../../ui/Button";
 
 const MiniProfile = ({ state, className, miniProfileRef }) => {
   const { chatbot } = useContext(ChatbotContext);
@@ -9,15 +13,15 @@ const MiniProfile = ({ state, className, miniProfileRef }) => {
   return (
     <section
       id="bot-profile"
-      className={`flex flex-col items-center ${className}`}
+      className={`flex flex-col items-center pt-10 ${className}`}
       ref={miniProfileRef}
     >
-      <img
+      <Image
         src={configuration.icon}
         alt="Chatbot Icon"
-        width={100}
-        height={100}
-        className="rounded-full mb-2 aspect-square object-cover select-none"
+        variant="icon"
+        size="lg"
+        className="mb-2"
       />
       <div
         id="details"
@@ -26,22 +30,46 @@ const MiniProfile = ({ state, className, miniProfileRef }) => {
         <h1 className="max-w-full font-bold text-2xl sm:text-4xl mb-1 dark:text-white capitalize line-clamp-5">
           {configuration.name}
         </h1>
-        <h3 className="max-w-full dark:text-white sm:text-lg mb-6 line-clamp-5">
+        <h3 className="max-w-full dark:text-white sm:text-lg mb-10 line-clamp-5">
           {configuration.slogan}
         </h3>
         {state && (
-          <p className="dark:text-white text-sm opacity-70 whitespace-pre-line"></p>
+          <div className="space-y-8">
+            <p className="max-w-[600px] text-left text-black/80 dark:text-white/80 text-sm whitespace-pre-line">
+              {chatbotConfig.about}
+            </p>
+            <section className="flex flex-col items-center gap-8 text-black/90 dark:text-white/90">
+              <h2 className="font-semibold text-xl">Meet my creators!</h2>
+              <div className="inline-grid grid-cols-2 gap-4">
+                <DevIcon
+                  to="https://moonbamiofficial.vercel.app/"
+                  src="https://github.com/moonbamijam.png"
+                  alt="Jam Moonbami"
+                  text="moonbami"
+                />
+                <DevIcon
+                  to="https://www.facebook.com/JirehTumbagahan"
+                  src="https://github.com/Jireh-sama.png"
+                  alt="Jireh"
+                  text="ecchiko"
+                />
+              </div>
+            </section>
+          </div>
         )}
       </div>
       {state && (
-        <Link
-          to="/"
-          className="hover:underline text-primary dark:text-primary-light capitalize mb-8"
-        >
-          back to homepage
+        <Link to="/" className="my-8">
+          <Button
+            variant="cta"
+            size="xl"
+            className="text-sm border border-surface dark:border-dm-surface bg-surface dark:bg-dm-surface hover:bg-primary dark:hover:bg-primary dark:text-white hover:text-white"
+          >
+            back to homepage
+          </Button>
         </Link>
       )}
-      <hr className="w-full" />
+      <hr className="w-full border-surface-dark dark:border-dm-surface-light" />
     </section>
   );
 };
