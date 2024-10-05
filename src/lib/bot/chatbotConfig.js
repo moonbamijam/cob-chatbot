64ea844b1caa875507676ed5c24fbf335c906762
MoonbamiOfficial <141120384+MoonbamiOfficial@users.noreply.github.com>
2024-10-05 12:56:49 +0800

    feat: add about to chatbot static config

--- src/lib/bot/chatbotConfig.js
+++ src/lib/bot/chatbotConfig.js
@@ -4,4 +4,6 @@ export const chatbotConfig = {
   name: "Vivienne",
   slogan: "Tanong mo, sagot ko, dahil at home ka dito!",
   introduction: "The interactive chatbot for the city of Bacoor, Cavite.",
+  about:
+    "Our capstone project that uses Natural Language Processing (NLP) to deliver smooth experience for the users. The bot is temporarily being hosted by the dummy website we created based on the official website of the Bacoor City Hall. We really hope you have enjoyed using our app! ",
 };
