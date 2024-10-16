f1a1cf054548aecc52326344fc8453393078a5a8
Jam Moonbami <141120384+moonbamijam@users.noreply.github.com>
2024-10-16 10:19:37 +0800

    feat: add skeleton screen ui (#145)
    * feat: add skeleton screen to landing page image
    
    * feat: add skeleton screen ui component and variant
    
    * feat: add skeleton screen to dev icon in settings
    
    * refactor: remove old skeleton screen ui for chats
    
    * impr: new chat skeleton screen
    
    ---------
    
    Co-authored-by: MoonbamiOfficial <141120384+MoonbamiOfficial@users.noreply.github.com>

---
+++ src/components/ui/SkeletonScreen.tsx
@@ -0,0 +1,26 @@
+import { forwardRef, HTMLAttributes, ImgHTMLAttributes } from "react";
+import { VariantProps } from "class-variance-authority";
+
+// utils
+import { cn } from "../../utils/cn";
+
+// library
+import { skeletonScreenVariants } from "../../lib/variants/skeletonScreenVariants";
+
+type SkeletonScreenProps = HTMLAttributes<HTMLDivElement> &
+  ImgHTMLAttributes<HTMLImageElement> &
+  VariantProps<typeof skeletonScreenVariants> & {};
+
+const SkeletonScreen = forwardRef<HTMLDivElement, SkeletonScreenProps>(
+  ({ className, variant, ...props }, ref) => {
+    return (
+      <div
+        ref={ref}
+        className={cn(skeletonScreenVariants({ className, variant }))}
+        {...props}
+      />
+    );
+  },
+);
+
+export default SkeletonScreen;
