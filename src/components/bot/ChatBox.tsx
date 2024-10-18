0d3d32faca61761411386665cc5625c3c9cc2642
MoonbamiOfficial <141120384+MoonbamiOfficial@users.noreply.github.com>
2024-10-18 21:32:52 +0800

    feat: show chat box at full screen in small screens

--- src/components/bot/ChatBox.tsx
+++ src/components/bot/ChatBox.tsx
@@ -41,8 +41,8 @@ const ChatBox = ({ closeUsing }: ChatBoxProps) => {
     <>
       <div
         id="message-box"
-        className={`w-full h-full xl:max-w-[500px] fixed flex flex-col items-center xl:right-[10%] xl:bottom-[8%] bg-background dark:bg-dm-background xl:rounded-xl overflow-hidden z-[100] ${
-          isChatActive ? "opacity-100 visible" : "opacity-0 invisible"
+        className={`w-full h-full xl:max-w-[500px] fixed flex flex-col items-center xl:right-[10%] xl:bottom-[8%] bg-background dark:bg-dm-background xl:rounded-xl overflow-hidden z-[100] opacity-100  ${
+          isChatActive ? "opacity-100 visible" : "xl:opacity-0 xl:invisible"
         } [@media_((min-height:0px)_and_(min-width:1280px))]:max-h-[85%]`}
         style={{
           fontFamily: fontFamily,
