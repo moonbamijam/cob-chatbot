d4477e2396f4f19b2b183ae530122c15cf2a96ea
Ecchiko <jirehtumbagahan@gmail.com>
2024-10-19 10:04:07 +0800

    feat: add regex function to check if a string contains image symbol

--- src/utils/splitMessage.ts
+++ src/utils/splitMessage.ts
@@ -1,6 +1,8 @@
 export const hasSymbol = (text: string) => /@=@/.test(text);
 let multipleMessage: string[] = [];
 
+export const hasImageSymbol = (text: string) => /\[([^\]]+)\]/.test(text);
+
 export const splitMessage = (text: string) => {
   if (hasSymbol(text)) {
     multipleMessage = text.split("@=@");
