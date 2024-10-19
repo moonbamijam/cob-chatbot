47ef65827513a550012593797fb6c2835f97c829
Ecchiko <jirehtumbagahan@gmail.com>
2024-10-19 10:02:44 +0800

    feat: add function to extract link from bot answer

--- src/utils/splitLink.ts
+++ src/utils/splitLink.ts
@@ -14,6 +14,24 @@ export const splitLinkToResponse = (text: string) => {
       matchLinkPattern,
       `<a href="${link[1]}" class="text-primary dark:text-secondary font-semibold uppercase hover:underline" target="_blank">click here</a>`,
     );
+
     return interpolatedString;
   } else return text;
+};
+
+export const extractLink = (
+  input: string,
+): { link: string | null; text: string } => {
+  // Regular expression to match text in square bracket
+  const regex = /\[([^\]]+)\]/;
+  const match = input.match(regex);
+
+  if (match) {
+    const link = match[1]; // Extract the link
+    // Remove the square brackets and link from the text
+    const text = input.replace(regex, "").trim();
+    return { link, text };
+  }
+
+  return { link: null, text: input.trim() };
 };
