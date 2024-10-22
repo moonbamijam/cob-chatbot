087054b60040cbbcc9d5bb4fe71d48b57ae76f90
MoonbamiOfficial <141120384+MoonbamiOfficial@users.noreply.github.com>
2024-10-22 17:02:12 +0800

    style: fix import to mini profile section component

--- src/components/sections/MiniProfile.tsx
+++ src/components/sections/MiniProfile.tsx
@@ -1,6 +1,6 @@
 import { Link } from "react-router-dom";
 import { LegacyRef, useContext } from "react";
-import { chatbotConfig } from "@src/lib/static/bot/chatbot-config";
+import { chatbotConfig } from "@constants/bot/chatbot-config";
 
 // context
 import { ChatbotContext } from "@contexts/ChatbotContext";
