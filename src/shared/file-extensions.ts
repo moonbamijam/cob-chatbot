3a1dd686a9d913007dd6cf0ea2ce3c80880654fd
MoonbamiOfficial <141120384+MoonbamiOfficial@users.noreply.github.com>
2024-10-31 09:50:52 +0800

    feat: add jfif file extension to images

--- src/shared/file-extensions.ts
+++ src/shared/file-extensions.ts
@@ -1,4 +1,4 @@
-export const images = ["png", "jpg", "jpeg", "gif", "tiff", "webp"];
+export const images = ["png", "jpg", "jpeg", "gif", "tiff", "webp", "jfif"];
 export const videos = ["mp4", "mov", "avi", "wmv", "avchd", "webm", "flv"];
 export const files = [
   "doc",
