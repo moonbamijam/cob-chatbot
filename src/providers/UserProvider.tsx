1f7b6a60b956c8018e26792decf5d652b7d66b7c
MoonbamiOfficial <141120384+MoonbamiOfficial@users.noreply.github.com>
2024-11-01 15:53:45 +0800

    feat: add rating to user provider and context

--- src/providers/UserProvider.tsx
+++ src/providers/UserProvider.tsx
@@ -1,4 +1,12 @@
-import { useContext, useMemo } from "react";
+import { useContext, useEffect, useMemo, useState } from "react";
+import {
+  average,
+  doc,
+  getAggregateFromServer,
+  getDoc,
+  updateDoc,
+} from "firebase/firestore";
+
 // contexts
 import { UserContext } from "@contexts/UserContext";
 import { ChatbotContext } from "@contexts/ChatbotContext";
@@ -6,17 +14,66 @@ import { ChatbotContext } from "@contexts/ChatbotContext";
 // utils
 import { verifiedUID } from "@utils/uid";
 
+// refs
+import { usersCollectionRef } from "@shared/collection-refs";
+
+const getLocalRating = () => {
+  const rating = parseInt(localStorage.getItem("rating") || "");
+  return rating ? rating : 0;
+};
+
 const UserProvider = ({ children }: { children: React.ReactNode }) => {
   const uid = verifiedUID();
   const chatbot = useContext(ChatbotContext);
   const { conversation } = chatbot.conversation;
 
+  const [rating, setRating] = useState<number>(getLocalRating);
+  const [averageRating, setAverageRating] = useState<number>(Number);
+  const [hasRated, setHasRated] = useState<boolean>(true);
+
+  const getAverageRating = async () => {
+    try {
+      const totalRatings = await getAggregateFromServer(usersCollectionRef, {
+        rating: average("rating"),
+      });
+      const result = totalRatings.data().rating?.toFixed(2);
+      if (result) setAverageRating(parseFloat(result));
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const getRatingFromFirestore = async () => {
+    const ratingdoc = await getDoc(doc(usersCollectionRef, uid));
+    localStorage.setItem("rating", ratingdoc.data()?.rating);
+    setHasRated(true);
+  };
+
+  const postRating = async () => {
+    if (rating) {
+      await updateDoc(doc(usersCollectionRef, uid), {
+        rating: rating,
+      });
+    }
+  };
+
+  // listener
+  useEffect(() => {
+    postRating();
+    getRatingFromFirestore();
+  }, [rating]);
+
+  useEffect(() => {
+    getAverageRating();
+  }, []);
+
   const user = useMemo(() => {
     return {
       uid: uid,
       conversation: conversation,
+      rating: { rating, setRating, averageRating, hasRated, setHasRated },
     };
-  }, [uid, conversation]);
+  }, [uid, conversation, rating, averageRating, hasRated]);
 
   return <UserContext.Provider value={user}>{children}</UserContext.Provider>;
 };
