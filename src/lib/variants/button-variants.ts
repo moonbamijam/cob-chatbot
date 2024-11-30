18fadb07ebe992c1d64959becf9d3ed0ae38b4a5
MoonbamiOfficial <141120384+MoonbamiOfficial@users.noreply.github.com>
2024-12-01 07:45:43 +0800

    feat: add empty variant to button variants

--- src/lib/variants/button-variants.ts
+++ src/lib/variants/button-variants.ts
@@ -16,6 +16,7 @@ export const buttonVariants = cva("w-full h-full select-none", {
         "bg-surface has-[input]:hover:bg-primary [&>label]:hover:text-white has-[input]:dark:hover:bg-primary dark:bg-dm-surface [&>label]:dark:text-white has-[:checked]:bg-primary has-[:checked]:dark:bg-primary has-[:checked]:hover:bg-primary-dark has-[:checked]:text-white shadow-md px-4 py-3 flex justify-center items-center rounded-3xl",
       settings:
         "flex rounded-lg text-left font-helvetica-bold hover:bg-gray-300 dark:hover:bg-dm-surface [&>span]:text-black/60 [&>span]:dark:text-white/60 [&>span]:has-[:checked]:text-white [&>span]:has-[:checked]:dark:text-white [&>div>svg>path]:text-black/60 [&>div>svg>path]:dark:text-white/60 [&>div>svg>path]:has-[:checked]:text-white [&>div>svg>path]:has-[:checked]:dark:text-white [&>div>svg>circle]:text-black/60 [&>div>svg>circle]:dark:text-white/60 [&>div>svg>circle]:has-[:checked]:text-white [&>div>svg>circle]:has-[:checked]:dark:text-white has-[:checked]:bg-primary has-[:checked]:hover:bg-primary has-[:checked]:dark:hover:bg-primary capitalize px-3 xs:px-4 py-3 cursor-pointer truncate",
+      empty: "",
     },
     size: {
       default: "w-max h-max",
